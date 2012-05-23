c6c300eecdd894e0d1204291ce57e402af4fb749
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-05-23 06:56:46 +0000

    Format
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@36534 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/internal/web/client/PlaceRequestParams.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/internal/web/client/PlaceRequestParams.java
@@ -2,8 +2,7 @@ package org.siemac.metamac.internal.web.client;
 
 public class PlaceRequestParams {
 
-    public static final String dsdParam = "dsdId";
+    public static final String dsdParam           = "dsdId";
     public static final String conceptSchemeParam = "schId";
-    
 
 }
