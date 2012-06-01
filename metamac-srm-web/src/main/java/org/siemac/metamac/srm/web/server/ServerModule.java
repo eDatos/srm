fa6d4b596bdd92fc2f60fd5395f8fa539b90a5ce
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-01 13:07:48 +0000

    Mock CAS
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@37032 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/ServerModule.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/ServerModule.java
@@ -47,8 +47,10 @@ import org.siemac.metamac.srm.web.shared.SaveDescriptorForDsdAction;
 import org.siemac.metamac.srm.web.shared.SaveDsdAction;
 import org.siemac.metamac.web.common.server.handlers.CloseSessionActionHandler;
 import org.siemac.metamac.web.common.server.handlers.GetLoginPageUrlActionHandler;
+import org.siemac.metamac.web.common.server.handlers.MockCASUserActionHandler;
 import org.siemac.metamac.web.common.shared.CloseSessionAction;
 import org.siemac.metamac.web.common.shared.GetLoginPageUrlAction;
+import org.siemac.metamac.web.common.shared.MockCASUserAction;
 import org.siemac.metamac.web.common.shared.ValidateTicketAction;
 import org.springframework.stereotype.Component;
 
@@ -90,6 +92,9 @@ public class ServerModule extends HandlerModule {
         bindHandler(ValidateTicketAction.class, ValidateTicketActionHandler.class);
         bindHandler(GetLoginPageUrlAction.class, GetLoginPageUrlActionHandler.class);
         bindHandler(CloseSessionAction.class, CloseSessionActionHandler.class);
+
+        // This action should be removed to use CAS authentication
+        bindHandler(MockCASUserAction.class, MockCASUserActionHandler.class);
     }
 
 }
