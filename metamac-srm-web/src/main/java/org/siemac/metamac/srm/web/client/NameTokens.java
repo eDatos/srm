320702bfd1b7b1f65f5ce51d9c0906e707626517
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-12 10:23:58 +0000

    NameTokens
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@37439 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/NameTokens.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/NameTokens.java
@@ -4,18 +4,20 @@ public class NameTokens {
 
     // Main pages
     // public static final String signInPage = "!signInPage";
-    public static final String structuralResourcesPage   = "!StructuralResourcesPage";
-    public static final String errorPage                 = "!ErrorPage";
-    public static final String mainPage                  = "!MainPage";
+    public static final String structuralResourcesPage = "!StructuralResourcesPage";
+    public static final String mainPage                = "!MainPage";
+
+    public static final String errorPage               = "!ErrorPage";
+    public static final String unauthorizedAccessPage  = "!UnauthorizedAccessPage";
 
     // DSDs
-    public static final String dsdListPage               = "!DsdListPage";
-    public static final String dsdPage                   = "!DsdPage";
-    public static final String dsdGeneralPage            = "!DsdGeneralPage";
-    public static final String dsdPrimaryMeasurePage     = "!DsdPrimaryMeasurePage";
-    public static final String dsdDimensionsPage         = "!DsdDimensionsPage";
-    public static final String dsdAttributesPage         = "!DsdAttributesPage";
-    public static final String dsdGroupKeysPage          = "!DsdGroupKeysPage";
+    public static final String dsdListPage             = "!DsdListPage";
+    public static final String dsdPage                 = "!DsdPage";
+    public static final String dsdGeneralPage          = "!DsdGeneralPage";
+    public static final String dsdPrimaryMeasurePage   = "!DsdPrimaryMeasurePage";
+    public static final String dsdDimensionsPage       = "!DsdDimensionsPage";
+    public static final String dsdAttributesPage       = "!DsdAttributesPage";
+    public static final String dsdGroupKeysPage        = "!DsdGroupKeysPage";
 
     // Concepts
     public static final String conceptSchemeListPage     = "!ConceptSchemeListPage";
