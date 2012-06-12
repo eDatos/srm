ee6ba4bb2d4af326f93960141a1fa564a6a1ae9d
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-12 10:39:07 +0000

    Gatekeeper
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@37444 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/MetamacPlaceManager.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/MetamacPlaceManager.java
@@ -8,9 +8,6 @@ import com.gwtplatform.mvp.client.proxy.PlaceManagerImpl;
 import com.gwtplatform.mvp.client.proxy.PlaceRequest;
 import com.gwtplatform.mvp.client.proxy.TokenFormatter;
 
-// see ClientModule
-// bindConstant().annotatedWith(DefaultPlace.class).to(NameTokens.signInPage);
-
 public class MetamacPlaceManager extends PlaceManagerImpl {
 
     private final PlaceRequest defaultPlaceRequest;
@@ -26,15 +23,12 @@ public class MetamacPlaceManager extends PlaceManagerImpl {
         revealPlace(defaultPlaceRequest);
     }
 
-    /*
-     * TODO Implement this method when authentication needed
-     * @Override
-     * public void revealUnauthorizedPlace(String unauthorizedHistoryToken) {
-     * PlaceRequest placeRequest = new PlaceRequest(NameTokens.signInPage);
-     * placeRequest = placeRequest.with("redirect", unauthorizedHistoryToken);
-     * revealPlace(placeRequest);
-     * }
-     */
+    @Override
+    public void revealUnauthorizedPlace(String unauthorizedHistoryToken) {
+        PlaceRequest placeRequest = new PlaceRequest(NameTokens.unauthorizedAccessPage);
+        placeRequest = placeRequest.with("redirect", unauthorizedHistoryToken);
+        revealPlace(placeRequest);
+    }
 
     @Override
     public void revealErrorPlace(String invalidHistoryToken) {
