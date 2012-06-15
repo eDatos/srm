0ed476e6523eba8ff01172379a0e2c2334ed218d
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-15 09:03:25 +0000

    NoGatekeeper en MainPagePresenter
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@37744 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/presenter/MainPagePresenter.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/presenter/MainPagePresenter.java
@@ -27,6 +27,7 @@ import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.ContentSlot;
 import com.gwtplatform.mvp.client.annotations.NameToken;
+import com.gwtplatform.mvp.client.annotations.NoGatekeeper;
 import com.gwtplatform.mvp.client.annotations.ProxyEvent;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.proxy.Place;
@@ -48,6 +49,7 @@ public class MainPagePresenter extends Presenter<MainPagePresenter.MainPageView,
 
     @ProxyStandard
     @NameToken(NameTokens.mainPage)
+    @NoGatekeeper
     public interface MainPageProxy extends Proxy<MainPagePresenter>, Place {
 
     }
