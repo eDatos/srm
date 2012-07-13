25964650ea62fa4768885a9bf9bd2b237f44916b
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-07-13 09:46:39 +0000

    IN PROGRESS - issue METAMAC-772: OptimisticLockingException al guardar componentes (ver Descripción) http://jira.arte-consultores.com/browse/METAMAC-772
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@38743 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/base/serviceapi/BaseServiceTest.java
+++ metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/base/serviceapi/BaseServiceTest.java
@@ -6,27 +6,17 @@ import static org.junit.Assert.assertTrue;
 import java.util.List;
 
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.siemac.metamac.common.test.mock.OracleJNDIMock;
-import org.siemac.metamac.domain.srm.dto.DataSetDto;
 import org.siemac.metamac.srm.core.base.domain.Component;
 import org.siemac.metamac.srm.core.base.domain.ComponentList;
 import org.siemac.metamac.srm.core.base.serviceapi.utils.BaseDoMocks;
 import org.siemac.metamac.srm.core.common.SrmBaseTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.jndi.SimpleNamingContextBuilder;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
-
-//@ContextConfiguration(locations = {"classpath:spring/srm-core/oracle/applicationContext-test.xml"})
-//@TransactionConfiguration(transactionManager = "txManagerCore", defaultRollback = true)
-//@Transactional
-//public class BaseServiceTest extends AbstractTransactionalJUnit4SpringContextTests implements SdmxBaseServiceTestBase {
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/srm/applicationContext-test.xml"})
@@ -39,20 +29,9 @@ public class BaseServiceTest extends SrmBaseTest implements SdmxBaseServiceTestB
 
     private final ServiceContext serviceContext               = new ServiceContext("system", "123456", "junit");
 
-//    private final String         organizationByDefaultLogicId = "METAMAC_ORGANISATION";
-
     protected ServiceContext getServiceContext() {
         return serviceContext;
     }
-    
-//    @BeforeClass
-//    public static void setUp() {
-//        // JNDI SDMXDS
-//        SimpleNamingContextBuilder simpleNamingContextBuilder = OracleJNDIMock.setUp("SDMXDS", "srm_test", "srm_test", "jdbc:oracle:thin:@localhost:1521:XE", null);
-//
-//        // JNDI tatisticDatasetDS
-////        simpleNamingContextBuilder = OracleJNDIMock.setUp("StatisticDatasetDS", "sdmx_data", "sdmx_data", "jdbc:oracle:thin:@localhost:1521:XE", simpleNamingContextBuilder);
-//    }
 
     /**************************************************************************
      * Component Tests
@@ -60,15 +39,12 @@ public class BaseServiceTest extends SrmBaseTest implements SdmxBaseServiceTestB
 
     @Test
     public void testSaveComponent() throws Exception {
-
         Component component = sdmxBaseService.saveComponent(getServiceContext(), BaseDoMocks.createDimension().get(0));
         assertNotNull(component);
-
     }
 
     @Test
     public void testDeleteComponent() throws Exception {
-
         testSaveComponent();
 
         List<Component> components = sdmxBaseService.findAllComponent(getServiceContext());
@@ -77,13 +53,11 @@ public class BaseServiceTest extends SrmBaseTest implements SdmxBaseServiceTestB
         sdmxBaseService.deleteComponent(getServiceContext(), component);
 
         assertTrue(sdmxBaseService.findAllComponent(getServiceContext()).size() < components.size());
-
     }
 
     @Override
     @Test
     public void testFindAllComponent() throws Exception {
-
         testSaveComponent();
 
         List<Component> components = sdmxBaseService.findAllComponent(getServiceContext());
@@ -95,17 +69,14 @@ public class BaseServiceTest extends SrmBaseTest implements SdmxBaseServiceTestB
      **************************************************************************/
     @Test
     public void testSaveComponentList() throws Exception {
-
         ComponentList componentList = sdmxBaseService.saveComponentList(getServiceContext(), BaseDoMocks.createDimensionDescriptor());
 
         assertNotNull(componentList);
-
     }
 
     @Override
     @Test
     public void testDeleteComponentList() throws Exception {
-
         testSaveComponentList();
 
         List<ComponentList> componentLists = sdmxBaseService.findAllComponentList(getServiceContext());
@@ -114,7 +85,6 @@ public class BaseServiceTest extends SrmBaseTest implements SdmxBaseServiceTestB
         sdmxBaseService.deleteComponentList(getServiceContext(), componentList);
 
         assertTrue(sdmxBaseService.findAllComponentList(getServiceContext()).size() < componentLists.size());
-
     }
 
     @Override
@@ -125,8 +95,6 @@ public class BaseServiceTest extends SrmBaseTest implements SdmxBaseServiceTestB
         List<ComponentList> componentLists = sdmxBaseService.findAllComponentList(getServiceContext());
         assertTrue(!componentLists.isEmpty());
     }
-
-
 
     @Override
     public void testFindComponentListById() throws Exception {
