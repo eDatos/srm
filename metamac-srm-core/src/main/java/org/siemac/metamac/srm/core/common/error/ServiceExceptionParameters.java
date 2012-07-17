31ecfbca4730c97a6b4cc91a4a109453dbf78fb9
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-07-17 13:54:45 +0000

    IN PROGRESS - issue METAMAC-823: Cambiar el uso de URI por el de URN correctamente http://jira.arte-consultores.com/browse/METAMAC-823
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@38853 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/error/ServiceExceptionParameters.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/error/ServiceExceptionParameters.java
@@ -2,29 +2,79 @@ package org.siemac.metamac.srm.core.common.error;
 
 public class ServiceExceptionParameters {
 
-    public static final String ID                         = "id";
+    public static final String ID                                              = "id";
 
     // MANTAINER
-    public static final String MANTAINER                  = "mantainer";
-    public static final String MANTAINER_CODE              = "mantainer.code";
-    public static final String MANTAINER_URN              = "mantainer.urn";
-    public static final String MANTAINER_TYPE             = "mantainer.type";
-    public static final String MANTAINER_URI              = "mantainer.uri";
-    public static final String MANTAINER_MANAGEMENTAPPURL = "mantainer.managementAppUrl";
-    public static final String MANTAINER_TITLE            = "mantainer.title";
+    public static final String MANTAINER                                       = "mantainer";
+    public static final String MANTAINER_CODE                                  = "mantainer.code";
+    public static final String MANTAINER_URN                                   = "mantainer.urn";
+    public static final String MANTAINER_TYPE                                  = "mantainer.type";
+    public static final String MANTAINER_URI                                   = "mantainer.uri";
+    public static final String MANTAINER_MANAGEMENTAPPURL                      = "mantainer.managementAppUrl";
+    public static final String MANTAINER_TITLE                                 = "mantainer.title";
 
     // ITEM_SCHEME
-    public static final String ITEM_SCHEME                = "item_scheme";
-    public static final String ITEM_SCHEME_ID_LOGIC       = "item_scheme.id";
-    public static final String ITEM_SCHEME_VERSION_LOGIC  = "item_scheme.version";
-    public static final String ITEM_SCHEME_NAME           = "item_scheme.name";
-    public static final String ITEM_SCHEM_FINAL_LOGIC     = "item_scheme.name";
+    public static final String ITEM_SCHEME                                     = "item_scheme";
+    public static final String ITEM_SCHEME_ID_LOGIC                            = "item_scheme.id";
+    public static final String ITEM_SCHEME_VERSION_LOGIC                       = "item_scheme.version";
+    public static final String ITEM_SCHEME_NAME                                = "item_scheme.name";
+    public static final String ITEM_SCHEM_FINAL_LOGIC                          = "item_scheme.name";
 
     // CONCEPT_SCHEME
-    public static final String CONCEPT_SCHEME             = "concept_scheme";
-    public static final String CONCEPT_SCHEME_ID          = "concept_scheme.id";
+    public static final String CONCEPT_SCHEME                                  = "concept_scheme";
+    public static final String CONCEPT_SCHEME_ID                               = "concept_scheme.id";
 
     // CONCEPT
-    public static final String CONCEPT                    = "concept";
+    public static final String CONCEPT                                         = "concept";
+
+    // ANNOTATION
+    public static final String ANNOTATION                                      = "annotation";
+    public static final String ANNOTATION_IDLOGIC                              = "annotation.idlogic";
+    public static final String ANNOTATION_TITLE                                = "annotation.title";
+    public static final String ANNOTATION_TYPE                                 = "annotation.type";
+    public static final String ANNOTATION_URL                                  = "annotation.url";
+    public static final String ANNOTATION_TEXT                                 = "annotation.text";
+
+    // ANNOTABLEARTEFACT
+    public static final String ANNOTABLEARTEFACT                               = "annotableartefact";
+
+    // NAMEABLE
+    public static final String NAMEABLEARTEFACT                                = "nameableartefact";
+    public static final String NAMEABLEARTEFACT_NAME                           = "nameableartefact.name";
+    public static final String NAMEABLEARTEFACT_DESCRIPTION                    = "nameableartefact.description";
+
+    // COMPONENT
+    public static final String COMPONENT                                       = "component";
+    public static final String COMPONENT_CONCEPTID                             = "component.conceptid";
+    public static final String COMPONENT_LOCALREPRESENTATION                   = "component.localrepresentation";
+    public static final String COMPONENT_TYPE                                  = "component.type";
+    public static final String COMPONENT_TYPE_DATA_ATTRIBUTE                   = "component.type.data_attribute";
+    public static final String COMPONENT_TYPE_DIMENSION_COMPONENT              = "component.type.dimension_component";
+    public static final String COMPONENT_TYPE_PRIMARY_MEASURE                  = "component.type.primary_measure";
+    public static final String COMPONENT_TYPE_TARGET_OBJECT                    = "component.type.target_object";
+    public static final String COMPONENT_TYPE_METADATA_ATTRIBUTE               = "component.type.metadata_attribute";
+    public static final String COMPONENT_REPRESENTATION_ENUMERATE_TITLE        = "component.representation.enumerate.title";
+
+    // DIMENSION
+    public static final String DIMENSION                                       = "dimension";
+    public static final String DIMENSION_ROLE_TITLE                            = "dimension.role.title";
+    public static final String DIMENSION_REPRESENTATION_ENUMERATE_TITLE        = "dimension.representation.enumerate.title";
+
+    // DATA_ATTRIBUTE
+    public static final String DATAATTRIBUTE                                   = "dataattribute";
+    public static final String DATAATTRIBUTE_USAGE_STATES                      = "dataattribute.usage_states";
+    public static final String DATAATTRIBUTE_RELATETO                          = "dataattribute.relateto";
+    public static final String DATAATTRIBUTE_ROLE                              = "dataattribute.role";
+    public static final String DATAATTRIBUTE_ROLE_TITLE                        = "dataattribute.role.title";
+    public static final String DATAATTRIBUTE_REPRESENTATION_ENUMERATE_TITLE    = "dataattribute.representation.enumerate.title";
+
+    // MEASUREDIMENSION
+    public static final String MEASUREDIMENSION                                = "measuredimension";
+    public static final String MEASUREDIMENSION_ROLE_TITLE                     = "measuredimension.role.title";
+    public static final String MEASUREDIMENSION_REPRESENTATION_ENUMERATE_TITLE = "measuredimension.representation.enumerate.title";
+
+    // TIMEDIMENSION
+    public static final String TIMEDIMENSION                                   = "timedimension";
+    public static final String TIMEDIMENSION_REPRESENTATION_ENUMERATE_TITLE    = "timedimension.representation.enumerate.title";
 
 }
