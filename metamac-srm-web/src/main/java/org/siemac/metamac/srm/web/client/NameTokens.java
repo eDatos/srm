24a9dc1e1fb8ce2957042ded217ba7365f5fba9a
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-07-18 07:59:13 +0000

    METAMAC-837 Cambio en los NameTokens
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@38871 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/NameTokens.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/NameTokens.java
@@ -3,26 +3,24 @@ package org.siemac.metamac.srm.web.client;
 public class NameTokens {
 
     // Main pages
-    public static final String structuralResourcesPage   = "structuralResources";
-    public static final String mainPage                  = "mainPage";
+    public static final String structuralResourcesPage = "structuralResources";
+    public static final String mainPage                = "mainPage";
 
-    public static final String errorPage                 = "error";
-    public static final String unauthorizedAccessPage    = "unauthorizedAccess";
+    public static final String errorPage               = "error";
+    public static final String unauthorizedAccessPage  = "unauthorizedAccess";
 
     // DSDs
-    public static final String dsdListPage               = "dsds";
-    public static final String dsdPage                   = "dsd";
-    public static final String dsdGeneralPage            = "general";
-    public static final String dsdPrimaryMeasurePage     = "primaryMeasure";
-    public static final String dsdDimensionsPage         = "dimensions";
-    public static final String dsdAttributesPage         = "attributes";
-    public static final String dsdGroupKeysPage          = "groupKeys";
+    public static final String dsdListPage             = "dsds";
+    public static final String dsdPage                 = "dsd";
+    public static final String dsdGeneralPage          = "general";
+    public static final String dsdPrimaryMeasurePage   = "primaryMeasure";
+    public static final String dsdDimensionsPage       = "dimensions";
+    public static final String dsdAttributesPage       = "attributes";
+    public static final String dsdGroupKeysPage        = "groupKeys";
 
     // Concepts
-    public static final String conceptSchemeListPage     = "conceptSchemes";
-    public static final String conceptSchemePage         = "conceptScheme";
-    public static final String conceptSchemeGeneralPage  = "conceptSchemeGeneral";
-    public static final String conceptSchemeConceptsPage = "conceptSchemeConcepts";
-    public static final String conceptPage               = "concept";
+    public static final String conceptSchemeListPage   = "conceptSchemes";
+    public static final String conceptSchemePage       = "conceptScheme";
+    public static final String conceptPage             = "concept";
 
 }
