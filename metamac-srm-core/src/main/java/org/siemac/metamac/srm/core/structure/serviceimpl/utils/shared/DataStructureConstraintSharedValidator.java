3f9d7c401ea09962e4da5bae275ce8b651b1eba3
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-07-24 08:10:40 +0000

    METAMAC-629 Modificacón de listados definidos en DataStructureConstraintSahredValidator (unmodifiable set)
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@39006 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/structure/serviceimpl/utils/shared/DataStructureConstraintSharedValidator.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/structure/serviceimpl/utils/shared/DataStructureConstraintSharedValidator.java
@@ -40,37 +40,32 @@ import static org.siemac.metamac.domain.srm.enume.domain.FacetValueTypeEnum.TIME
 import static org.siemac.metamac.domain.srm.enume.domain.FacetValueTypeEnum.TIME_FVT;
 import static org.siemac.metamac.domain.srm.enume.domain.FacetValueTypeEnum.URI_FVT;
 
-import org.apache.commons.lang.ArrayUtils;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.siemac.metamac.domain.srm.enume.domain.FacetValueTypeEnum;
 
 public class DataStructureConstraintSharedValidator {
 
-    public static final FacetValueTypeEnum[] simpleDataTypeValues = new FacetValueTypeEnum[]{STRING_FVT, ALPHA_FVT, ALPHA_NUMERIC_FVT, NUMERIC_FVT, BIG_INTEGER_FVT, INTEGER_FVT, LONG_FVT, SHORT_FVT,
-            DECIMAL_FVT, FLOAT_FVT, DOUBLE_FVT, BOOLEAN_FVT, URI_FVT, COUNT_FVT, INCLUSIVE_VALUE_RANGE_FVT, EXCLUSIVE_VALUE_RANGE_FVT, INCREMENTAL_FVT, OBSERVATIONAL_TIME_PERIOD_FVT,
-            STANDARD_TIME_PERIOD_FVT, BASIC_TIME_PERIOD_FVT, GREGORIAN_TIME_PERIOD_FVT, GREGORIAN_YEAR_FVT, GREGORIAN_YEAR_MONTH_FVT, GREGORIAN_DAY_FVT, REPORTING_TIME_PERIOD_FVT, REPORTING_YEAR_FVT,
-            REPORTING_SEMESTER_FVT, REPORTING_TRIMESTER_FVT, REPORTING_QUARTER_FVT, REPORTING_MONTH_FVT, REPORTING_WEEK_FVT, REPORTING_DAY_FVT, DATE_TIME_FVT, TIMES_RANGE_FVT, MONTH_FVT,
-            MONTH_DAY_FVT, DAY_FVT, TIME_FVT, DURATION_FVT};
+    public static final Set<FacetValueTypeEnum> simpleDataTypeValues = Collections.unmodifiableSet(new HashSet<FacetValueTypeEnum>(Arrays.asList(new FacetValueTypeEnum[]{STRING_FVT, ALPHA_FVT,
+            ALPHA_NUMERIC_FVT, NUMERIC_FVT, BIG_INTEGER_FVT, INTEGER_FVT, LONG_FVT, SHORT_FVT, DECIMAL_FVT, FLOAT_FVT, DOUBLE_FVT, BOOLEAN_FVT, URI_FVT, COUNT_FVT, INCLUSIVE_VALUE_RANGE_FVT,
+            EXCLUSIVE_VALUE_RANGE_FVT, INCREMENTAL_FVT, OBSERVATIONAL_TIME_PERIOD_FVT, STANDARD_TIME_PERIOD_FVT, BASIC_TIME_PERIOD_FVT, GREGORIAN_TIME_PERIOD_FVT, GREGORIAN_YEAR_FVT,
+            GREGORIAN_YEAR_MONTH_FVT, GREGORIAN_DAY_FVT, REPORTING_TIME_PERIOD_FVT, REPORTING_YEAR_FVT, REPORTING_SEMESTER_FVT, REPORTING_TRIMESTER_FVT, REPORTING_QUARTER_FVT, REPORTING_MONTH_FVT,
+            REPORTING_WEEK_FVT, REPORTING_DAY_FVT, DATE_TIME_FVT, TIMES_RANGE_FVT, MONTH_FVT, MONTH_DAY_FVT, DAY_FVT, TIME_FVT, DURATION_FVT})));
 
-    public static final FacetValueTypeEnum[] timeDataTypeValues   = new FacetValueTypeEnum[]{OBSERVATIONAL_TIME_PERIOD_FVT, STANDARD_TIME_PERIOD_FVT, BASIC_TIME_PERIOD_FVT, GREGORIAN_TIME_PERIOD_FVT,
-            GREGORIAN_YEAR_FVT, GREGORIAN_YEAR_MONTH_FVT, GREGORIAN_DAY_FVT, REPORTING_TIME_PERIOD_FVT, REPORTING_YEAR_FVT, REPORTING_SEMESTER_FVT, REPORTING_TRIMESTER_FVT, REPORTING_QUARTER_FVT,
-            REPORTING_MONTH_FVT, REPORTING_WEEK_FVT, REPORTING_DAY_FVT, DATE_TIME_FVT, TIMES_RANGE_FVT};
+    public static final Set<FacetValueTypeEnum> timeDataTypeValues   = Collections.unmodifiableSet(new HashSet<FacetValueTypeEnum>(Arrays.asList(new FacetValueTypeEnum[]{
+            OBSERVATIONAL_TIME_PERIOD_FVT, STANDARD_TIME_PERIOD_FVT, BASIC_TIME_PERIOD_FVT, GREGORIAN_TIME_PERIOD_FVT, GREGORIAN_YEAR_FVT, GREGORIAN_YEAR_MONTH_FVT, GREGORIAN_DAY_FVT,
+            REPORTING_TIME_PERIOD_FVT, REPORTING_YEAR_FVT, REPORTING_SEMESTER_FVT, REPORTING_TRIMESTER_FVT, REPORTING_QUARTER_FVT, REPORTING_MONTH_FVT, REPORTING_WEEK_FVT, REPORTING_DAY_FVT,
+            DATE_TIME_FVT, TIMES_RANGE_FVT                           })));
 
     public static boolean isSimpleDataType(FacetValueTypeEnum facetValueTypeEnum) {
-        if (facetValueTypeEnum != null) {
-            if (ArrayUtils.contains(simpleDataTypeValues, facetValueTypeEnum)) {
-                return true;
-            }
-        }
-        return false;
+        return facetValueTypeEnum != null && simpleDataTypeValues.contains(facetValueTypeEnum);
     }
 
     public static boolean isTimeDataType(FacetValueTypeEnum facetValueTypeEnum) {
-        if (facetValueTypeEnum != null) {
-            if (ArrayUtils.contains(timeDataTypeValues, facetValueTypeEnum)) {
-                return true;
-            }
-        }
-        return false;
+        return facetValueTypeEnum != null && timeDataTypeValues.contains(facetValueTypeEnum);
     }
 
 }
