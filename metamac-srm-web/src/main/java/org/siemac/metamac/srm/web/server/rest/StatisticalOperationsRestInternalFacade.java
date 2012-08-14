d88ff8ecae3ebf35d63e5d00730be6cf41d686c2
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-08-14 07:44:07 +0000

    METAMAC-948 Refactor en nombre de paquetes de la API de operaciones estadísticas
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@39547 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/rest/StatisticalOperationsRestInternalFacade.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/rest/StatisticalOperationsRestInternalFacade.java
@@ -1,7 +1,7 @@
 package org.siemac.metamac.srm.web.server.rest;
 
-import org.siemac.metamac.statistical_operations.rest.internal.v1_0.domain.Operation;
-import org.siemac.metamac.statistical_operations.rest.internal.v1_0.domain.Operations;
+import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.Operation;
+import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.Operations;
 import org.siemac.metamac.web.common.shared.exception.MetamacWebException;
 
 public interface StatisticalOperationsRestInternalFacade {
