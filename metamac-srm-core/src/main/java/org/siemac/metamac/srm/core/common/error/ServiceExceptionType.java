af35c0c043591cd837be20f08a060ef8a3bf0c5d
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-08-24 11:41:56 +0000

    METAMAC-871 Añadidos mensajes de error en ServiceExceptionType
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@39783 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/error/ServiceExceptionType.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/error/ServiceExceptionType.java
@@ -4,6 +4,8 @@ import org.siemac.metamac.core.common.exception.CommonServiceExceptionType;
 
 public class ServiceExceptionType extends com.arte.statistic.sdmx.srm.core.common.error.ServiceExceptionType {
 
-    public static final CommonServiceExceptionType CONCEPT_SCHEME_WRONG_PROC_STATUS       = create("exception.srm.concepts.concept_scheme.wrong_proc_status");
+    public static final CommonServiceExceptionType CONCEPT_SCHEME_WRONG_PROC_STATUS = create("exception.srm.concepts.concept_scheme.wrong_proc_status");
+
+    public static final CommonServiceExceptionType SECURITY_ACTION_NOT_ALLOWED      = create("exception.srm.security.action_not_allowed");
 
 }
