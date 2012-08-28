19249bd392e9e60cbbcca26fe96e8184b210bd25
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-08-28 12:59:54 +0000

    METAMAC-1001 Dto2DoMapper de SDMX
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@39870 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/error/ServiceExceptionParameters.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/error/ServiceExceptionParameters.java
@@ -10,7 +10,14 @@ public class ServiceExceptionParameters extends com.arte.statistic.sdmx.srm.core
     public static final String CONCEPT_SCHEME_RELATED_OPERATION  = CONCEPT_SCHEME + ".related_operation";
 
     // CONCEPT
+    public static final String CONCEPT_PLURAL_NAME               = CONCEPT + ".plural_name";
+    public static final String CONCEPT_ACRONYM                   = CONCEPT + ".acronym";
+    public static final String CONCEPT_DESCRIPTION_SOURCE        = CONCEPT + ".description_source";
+    public static final String CONCEPT_CONTEXT                   = CONCEPT + ".context";
+    public static final String CONCEPT_DOC_METHOD                = CONCEPT + ".doc_method";
     public static final String CONCEPT_SDMX_RELATED_ARTEFACT     = CONCEPT + ".sdmx_related_artefact";
+    public static final String CONCEPT_DERIVATION                = CONCEPT + ".derivation";
+    public static final String CONCEPT_LEGAL_ACTS                = CONCEPT + ".legal_acts";
 
     // LIFECYCLE
     public static final String PROC_STATUS_DRAFT                 = ItemSchemeMetamacProcStatusEnum.DRAFT.name();
