c2ff8e8ae572381dfdd4b11beea546ef266b35f0
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-08-29 08:06:51 +0000

    METAMAC-854 Búsqueda de esquemas de conceptos
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@39888 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/concept/GetConceptSchemePaginatedListActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/concept/GetConceptSchemePaginatedListActionHandler.java
@@ -1,12 +1,23 @@
 package org.siemac.metamac.srm.web.server.handlers.concept;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.siemac.metamac.core.common.criteria.MetamacCriteria;
+import org.siemac.metamac.core.common.criteria.MetamacCriteriaOrder;
+import org.siemac.metamac.core.common.criteria.MetamacCriteriaOrder.OrderTypeEnum;
+import org.siemac.metamac.core.common.criteria.MetamacCriteriaPaginator;
+import org.siemac.metamac.core.common.criteria.MetamacCriteriaResult;
 import org.siemac.metamac.core.common.exception.MetamacException;
-import org.siemac.metamac.srm.web.server.mock.ConceptSchemeService;
-import org.siemac.metamac.srm.web.server.mock.ConceptSchemeService.ConceptSchemePage;
+import org.siemac.metamac.srm.core.concept.dto.ConceptSchemeMetamacDto;
+import org.siemac.metamac.srm.core.criteria.ConceptSchemeVersionMetamacCriteriaOrderEnum;
+import org.siemac.metamac.srm.core.facade.serviceapi.SrmCoreServiceFacade;
 import org.siemac.metamac.srm.web.shared.concept.GetConceptSchemePaginatedListAction;
 import org.siemac.metamac.srm.web.shared.concept.GetConceptSchemePaginatedListResult;
+import org.siemac.metamac.web.common.server.ServiceContextHolder;
 import org.siemac.metamac.web.common.server.handlers.SecurityActionHandler;
 import org.siemac.metamac.web.common.server.utils.WebExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.gwtplatform.dispatch.shared.ActionException;
@@ -14,18 +25,31 @@ import com.gwtplatform.dispatch.shared.ActionException;
 @Component
 public class GetConceptSchemePaginatedListActionHandler extends SecurityActionHandler<GetConceptSchemePaginatedListAction, GetConceptSchemePaginatedListResult> {
 
+    @Autowired
+    private SrmCoreServiceFacade srmCoreServiceFacade;
+
     public GetConceptSchemePaginatedListActionHandler() {
         super(GetConceptSchemePaginatedListAction.class);
     }
 
     @Override
     public GetConceptSchemePaginatedListResult executeSecurityAction(GetConceptSchemePaginatedListAction action) throws ActionException {
-        // TODO: replace mock
-        // TODO ORDER BY last_update
+        MetamacCriteria criteria = new MetamacCriteria();
+        // Order
+        MetamacCriteriaOrder order = new MetamacCriteriaOrder();
+        order.setType(OrderTypeEnum.DESC);
+        order.setPropertyName(ConceptSchemeVersionMetamacCriteriaOrderEnum.LAST_UPDATED.name());
+        List<MetamacCriteriaOrder> criteriaOrders = new ArrayList<MetamacCriteriaOrder>();
+        criteriaOrders.add(order);
+        criteria.setOrdersBy(criteriaOrders);
+        // Pagination
+        criteria.setPaginator(new MetamacCriteriaPaginator());
+        criteria.getPaginator().setFirstResult(action.getFirstResult());
+        criteria.getPaginator().setMaximumResultSize(action.getMaxResults());
+        criteria.getPaginator().setCountTotalResults(true);
         try {
-            ConceptSchemePage page = ConceptSchemeService.findAllConceptSchemes(action.getFirstResult(), action.getMaxResults());
-            int pageNumber = (action.getFirstResult() / action.getMaxResults()) + 1;
-            return new GetConceptSchemePaginatedListResult(page.resultList, pageNumber, page.totalResults);
+            MetamacCriteriaResult<ConceptSchemeMetamacDto> result = srmCoreServiceFacade.findConceptSchemesByCondition(ServiceContextHolder.getCurrentServiceContext(), criteria);
+            return new GetConceptSchemePaginatedListResult(result.getResults(), result.getPaginatorResult().getFirstResult(), result.getPaginatorResult().getTotalResults());
         } catch (MetamacException e) {
             throw WebExceptionUtils.createMetamacWebException(e);
         }
