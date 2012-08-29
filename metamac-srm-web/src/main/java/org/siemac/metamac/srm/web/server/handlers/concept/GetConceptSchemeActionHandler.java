4742d676e7b0334a47bc010f7bcc69ca200acc1e
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-08-29 11:20:21 +0000

    METAMAC-890 Integración con la fachada de servicios
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@39908 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/concept/GetConceptSchemeActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/concept/GetConceptSchemeActionHandler.java
@@ -1,12 +1,14 @@
 package org.siemac.metamac.srm.web.server.handlers.concept;
 
-import org.siemac.metamac.core.common.dto.InternationalStringDto;
-import org.siemac.metamac.core.common.dto.LocalisedStringDto;
+import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.srm.core.concept.dto.ConceptSchemeMetamacDto;
-import org.siemac.metamac.srm.core.enume.domain.ItemSchemeMetamacProcStatusEnum;
+import org.siemac.metamac.srm.core.facade.serviceapi.SrmCoreServiceFacade;
 import org.siemac.metamac.srm.web.shared.concept.GetConceptSchemeAction;
 import org.siemac.metamac.srm.web.shared.concept.GetConceptSchemeResult;
+import org.siemac.metamac.web.common.server.ServiceContextHolder;
 import org.siemac.metamac.web.common.server.handlers.SecurityActionHandler;
+import org.siemac.metamac.web.common.server.utils.WebExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.gwtplatform.dispatch.shared.ActionException;
@@ -14,26 +16,21 @@ import com.gwtplatform.dispatch.shared.ActionException;
 @Component
 public class GetConceptSchemeActionHandler extends SecurityActionHandler<GetConceptSchemeAction, GetConceptSchemeResult> {
 
+    @Autowired
+    private SrmCoreServiceFacade srmCoreServiceFacade;
+
     public GetConceptSchemeActionHandler() {
         super(GetConceptSchemeAction.class);
     }
 
     @Override
     public GetConceptSchemeResult executeSecurityAction(GetConceptSchemeAction action) throws ActionException {
-        InternationalStringDto name = new InternationalStringDto();
-        LocalisedStringDto localisedStringDto = new LocalisedStringDto();
-        localisedStringDto.setId(2L);
-        localisedStringDto.setLabel("Esquema de conceptos 0001");
-        localisedStringDto.setLocale("es");
-        name.addText(localisedStringDto);
-
-        ConceptSchemeMetamacDto scheme = new ConceptSchemeMetamacDto();
-        scheme.setName(name);
-        scheme.setUrn("urn:sdmx:org.sdmx.infomodel.conceptscheme.ConceptScheme=SDMX:CROSS_DOMAIN_CONCEPTS(1.0)");
-        scheme.setCode("CROSS_DOMAIN_CONCEPTS");
-        scheme.setVersionLogic("1.0");
-        scheme.setProcStatus(ItemSchemeMetamacProcStatusEnum.DRAFT);
-        return new GetConceptSchemeResult(scheme);
+        try {
+            ConceptSchemeMetamacDto conceptSchemeMetamacDto = srmCoreServiceFacade.retrieveConceptSchemeByUrn(ServiceContextHolder.getCurrentServiceContext(), action.getUrn());
+            return new GetConceptSchemeResult(conceptSchemeMetamacDto);
+        } catch (MetamacException e) {
+            throw WebExceptionUtils.createMetamacWebException(e);
+        }
     }
 
 }
--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/concept/SaveConceptSchemeActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/concept/SaveConceptSchemeActionHandler.java
@@ -2,17 +2,22 @@ package org.siemac.metamac.srm.web.server.handlers.concept;
 
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.srm.core.concept.dto.ConceptSchemeMetamacDto;
-import org.siemac.metamac.srm.web.server.mock.ConceptSchemeService;
+import org.siemac.metamac.srm.core.facade.serviceapi.SrmCoreServiceFacade;
 import org.siemac.metamac.srm.web.shared.concept.SaveConceptSchemeAction;
 import org.siemac.metamac.srm.web.shared.concept.SaveConceptSchemeResult;
+import org.siemac.metamac.web.common.server.ServiceContextHolder;
 import org.siemac.metamac.web.common.server.handlers.SecurityActionHandler;
 import org.siemac.metamac.web.common.server.utils.WebExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.gwtplatform.dispatch.shared.ActionException;
 
 @Component
 public class SaveConceptSchemeActionHandler extends SecurityActionHandler<SaveConceptSchemeAction, SaveConceptSchemeResult> {
+
+    @Autowired
+    private SrmCoreServiceFacade srmCoreServiceFacade;
 
     public SaveConceptSchemeActionHandler() {
         super(SaveConceptSchemeAction.class);
@@ -21,8 +26,17 @@ public class SaveConceptSchemeActionHandler extends SecurityActionHandler<SaveCo
     @Override
     public SaveConceptSchemeResult executeSecurityAction(SaveConceptSchemeAction action) throws ActionException {
         try {
-            ConceptSchemeMetamacDto schemeDto = ConceptSchemeService.saveConceptScheme(action.getConceptSchemeDto());
-            return new SaveConceptSchemeResult(schemeDto);
+            ConceptSchemeMetamacDto conceptSchemeToSave = action.getConceptSchemeDto();
+            ConceptSchemeMetamacDto savedConceptSchemeDto = null;
+            if (conceptSchemeToSave.getId() == null) {
+                // Create
+                savedConceptSchemeDto = srmCoreServiceFacade.createConceptScheme(ServiceContextHolder.getCurrentServiceContext(), conceptSchemeToSave);
+                return new SaveConceptSchemeResult(savedConceptSchemeDto);
+            } else {
+                // Update
+                savedConceptSchemeDto = srmCoreServiceFacade.updateConceptScheme(ServiceContextHolder.getCurrentServiceContext(), action.getConceptSchemeDto());
+            }
+            return new SaveConceptSchemeResult(savedConceptSchemeDto);
         } catch (MetamacException e) {
             throw WebExceptionUtils.createMetamacWebException(e);
         }
