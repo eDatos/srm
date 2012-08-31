3759a90b7373aefc442eaf67aaa9051de2a31ee6
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-08-31 06:20:43 +0000

    in progress: METAMAC-984: Completar todos los metadatos de Concept. Metadato "relatedConcepts".
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@39969 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/error/ServiceExceptionType.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/error/ServiceExceptionType.java
@@ -6,6 +6,7 @@ public class ServiceExceptionType extends com.arte.statistic.sdmx.srm.core.commo
 
     public static final CommonServiceExceptionType CONCEPT_SCHEME_WRONG_PROC_STATUS = create("exception.srm.concepts.concept_scheme.wrong_proc_status");
     public static final CommonServiceExceptionType CONCEPT_TYPE_NOT_FOUND           = create("exception.srm.concepts.concept_type.not_found");
+    public static final CommonServiceExceptionType CONCEPT_WITH_RELATED_CONCEPTS    = create("exception.srm.concepts.related_concepts");
     public static final CommonServiceExceptionType SECURITY_ACTION_NOT_ALLOWED      = create("exception.srm.security.action_not_allowed");
 
 }
