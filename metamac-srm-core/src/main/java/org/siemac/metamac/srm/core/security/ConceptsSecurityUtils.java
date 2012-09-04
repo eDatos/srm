25468b3ea65976f1a5a25ab65c272408ce60c8fe
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-09-04 07:42:12 +0000

    METAMAC-871: Seguridad de usuarios en los Facade. ConceptsSecurityUtils recibe entidad en lugar de Dto para evitar hacer transformaciones
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@40041 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/security/ConceptsSecurityUtils.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/security/ConceptsSecurityUtils.java
@@ -3,9 +3,7 @@ package org.siemac.metamac.srm.core.security;
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.srm.core.common.error.ServiceExceptionType;
-import org.siemac.metamac.srm.core.concept.dto.ConceptSchemeMetamacDto;
-import org.siemac.metamac.srm.core.concept.enume.domain.ConceptSchemeTypeEnum;
-import org.siemac.metamac.srm.core.enume.domain.ItemSchemeMetamacProcStatusEnum;
+import org.siemac.metamac.srm.core.concept.domain.ConceptSchemeVersionMetamac;
 import org.siemac.metamac.srm.core.security.shared.SharedConceptsSecurityUtils;
 import org.siemac.metamac.sso.client.MetamacPrincipal;
 import org.siemac.metamac.sso.client.SsoClientConstants;
@@ -34,15 +32,14 @@ public class ConceptsSecurityUtils {
         }
     }
 
-    public static void canUpdateConceptScheme(ServiceContext ctx, ConceptSchemeMetamacDto conceptSchemeMetamacDto) throws MetamacException {
-        if (!SharedConceptsSecurityUtils.canUpdateConceptScheme(getMetamacPrincipal(ctx), getProcStatus(conceptSchemeMetamacDto), getType(conceptSchemeMetamacDto),
-                getOperacionCode(conceptSchemeMetamacDto))) {
+    public static void canUpdateConceptScheme(ServiceContext ctx, ConceptSchemeVersionMetamac conceptSchemeMetamac) throws MetamacException {
+        if (!SharedConceptsSecurityUtils.canUpdateConceptScheme(getMetamacPrincipal(ctx), conceptSchemeMetamac.getProcStatus(), conceptSchemeMetamac.getType(), getOperacionCode(conceptSchemeMetamac))) {
             throw new MetamacException(ServiceExceptionType.SECURITY_ACTION_NOT_ALLOWED, ctx.getUserId());
         }
     }
 
-    public static void canDeleteConceptScheme(ServiceContext ctx, ConceptSchemeMetamacDto conceptSchemeMetamacDto) throws MetamacException {
-        if (!SharedConceptsSecurityUtils.canDeleteConceptScheme(getMetamacPrincipal(ctx), getType(conceptSchemeMetamacDto), getOperacionCode(conceptSchemeMetamacDto))) {
+    public static void canDeleteConceptScheme(ServiceContext ctx, ConceptSchemeVersionMetamac conceptSchemeMetamac) throws MetamacException {
+        if (!SharedConceptsSecurityUtils.canDeleteConceptScheme(getMetamacPrincipal(ctx), conceptSchemeMetamac.getType(), getOperacionCode(conceptSchemeMetamac))) {
             throw new MetamacException(ServiceExceptionType.SECURITY_ACTION_NOT_ALLOWED, ctx.getUserId());
         }
     }
@@ -53,51 +50,51 @@ public class ConceptsSecurityUtils {
         }
     }
 
-    public static void canSendConceptSchemeToProductionValidation(ServiceContext ctx, ConceptSchemeMetamacDto conceptSchemeMetamacDto) throws MetamacException {
-        if (!SharedConceptsSecurityUtils.canSendConceptSchemeToProductionValidation(getMetamacPrincipal(ctx), getType(conceptSchemeMetamacDto), getOperacionCode(conceptSchemeMetamacDto))) {
+    public static void canSendConceptSchemeToProductionValidation(ServiceContext ctx, ConceptSchemeVersionMetamac conceptSchemeMetamac) throws MetamacException {
+        if (!SharedConceptsSecurityUtils.canSendConceptSchemeToProductionValidation(getMetamacPrincipal(ctx), conceptSchemeMetamac.getType(), getOperacionCode(conceptSchemeMetamac))) {
             throw new MetamacException(ServiceExceptionType.SECURITY_ACTION_NOT_ALLOWED, ctx.getUserId());
         }
     }
 
-    public static void canSendConceptSchemeToDiffusionValidation(ServiceContext ctx, ConceptSchemeMetamacDto conceptSchemeMetamacDto) throws MetamacException {
-        if (!SharedConceptsSecurityUtils.canSendConceptSchemeToDiffusionValidation(getMetamacPrincipal(ctx), getType(conceptSchemeMetamacDto), getOperacionCode(conceptSchemeMetamacDto))) {
+    public static void canSendConceptSchemeToDiffusionValidation(ServiceContext ctx, ConceptSchemeVersionMetamac conceptSchemeMetamac) throws MetamacException {
+        if (!SharedConceptsSecurityUtils.canSendConceptSchemeToDiffusionValidation(getMetamacPrincipal(ctx), conceptSchemeMetamac.getType(), getOperacionCode(conceptSchemeMetamac))) {
             throw new MetamacException(ServiceExceptionType.SECURITY_ACTION_NOT_ALLOWED, ctx.getUserId());
         }
     }
 
-    public static void canRejectConceptSchemeValidation(ServiceContext ctx, ConceptSchemeMetamacDto conceptSchemeMetamacDto) throws MetamacException {
-        if (!SharedConceptsSecurityUtils.canRejectConceptSchemeValidation(getMetamacPrincipal(ctx), getProcStatus(conceptSchemeMetamacDto), getType(conceptSchemeMetamacDto),
-                getOperacionCode(conceptSchemeMetamacDto))) {
+    public static void canRejectConceptSchemeValidation(ServiceContext ctx, ConceptSchemeVersionMetamac conceptSchemeMetamac) throws MetamacException {
+        if (!SharedConceptsSecurityUtils.canRejectConceptSchemeValidation(getMetamacPrincipal(ctx), conceptSchemeMetamac.getProcStatus(), conceptSchemeMetamac.getType(),
+                getOperacionCode(conceptSchemeMetamac))) {
             throw new MetamacException(ServiceExceptionType.SECURITY_ACTION_NOT_ALLOWED, ctx.getUserId());
         }
     }
 
-    public static void canPublishConceptSchemeInternally(ServiceContext ctx, ConceptSchemeMetamacDto conceptSchemeMetamacDto) throws MetamacException {
-        if (!SharedConceptsSecurityUtils.canPublishConceptSchemeInternally(getMetamacPrincipal(ctx), getType(conceptSchemeMetamacDto), getOperacionCode(conceptSchemeMetamacDto))) {
+    public static void canPublishConceptSchemeInternally(ServiceContext ctx, ConceptSchemeVersionMetamac conceptSchemeMetamac) throws MetamacException {
+        if (!SharedConceptsSecurityUtils.canPublishConceptSchemeInternally(getMetamacPrincipal(ctx), conceptSchemeMetamac.getType(), getOperacionCode(conceptSchemeMetamac))) {
             throw new MetamacException(ServiceExceptionType.SECURITY_ACTION_NOT_ALLOWED, ctx.getUserId());
         }
     }
 
-    public static void canPublishConceptSchemeExternally(ServiceContext ctx, ConceptSchemeMetamacDto conceptSchemeMetamacDto) throws MetamacException {
-        if (!SharedConceptsSecurityUtils.canPublishConceptSchemeExternally(getMetamacPrincipal(ctx), getType(conceptSchemeMetamacDto), getOperacionCode(conceptSchemeMetamacDto))) {
+    public static void canPublishConceptSchemeExternally(ServiceContext ctx, ConceptSchemeVersionMetamac conceptSchemeMetamac) throws MetamacException {
+        if (!SharedConceptsSecurityUtils.canPublishConceptSchemeExternally(getMetamacPrincipal(ctx), conceptSchemeMetamac.getType(), getOperacionCode(conceptSchemeMetamac))) {
             throw new MetamacException(ServiceExceptionType.SECURITY_ACTION_NOT_ALLOWED, ctx.getUserId());
         }
     }
 
-    public static void canVersioningConceptScheme(ServiceContext ctx, ConceptSchemeMetamacDto conceptSchemeMetamacDto) throws MetamacException {
-        if (!SharedConceptsSecurityUtils.canVersioningConceptScheme(getMetamacPrincipal(ctx), getType(conceptSchemeMetamacDto), getOperacionCode(conceptSchemeMetamacDto))) {
+    public static void canVersioningConceptScheme(ServiceContext ctx, ConceptSchemeVersionMetamac conceptSchemeMetamac) throws MetamacException {
+        if (!SharedConceptsSecurityUtils.canVersioningConceptScheme(getMetamacPrincipal(ctx), conceptSchemeMetamac.getType(), getOperacionCode(conceptSchemeMetamac))) {
             throw new MetamacException(ServiceExceptionType.SECURITY_ACTION_NOT_ALLOWED, ctx.getUserId());
         }
     }
 
-    public static void canAnnounceConceptScheme(ServiceContext ctx, ConceptSchemeMetamacDto conceptSchemeMetamacDto) throws MetamacException {
-        if (!SharedConceptsSecurityUtils.canAnnounceConceptScheme(getMetamacPrincipal(ctx), getType(conceptSchemeMetamacDto), getOperacionCode(conceptSchemeMetamacDto))) {
+    public static void canAnnounceConceptScheme(ServiceContext ctx, ConceptSchemeVersionMetamac conceptSchemeMetamac) throws MetamacException {
+        if (!SharedConceptsSecurityUtils.canAnnounceConceptScheme(getMetamacPrincipal(ctx), conceptSchemeMetamac.getType(), getOperacionCode(conceptSchemeMetamac))) {
             throw new MetamacException(ServiceExceptionType.SECURITY_ACTION_NOT_ALLOWED, ctx.getUserId());
         }
     }
 
-    public static void canCancelConceptSchemeValidity(ServiceContext ctx, ConceptSchemeMetamacDto conceptSchemeMetamacDto) throws MetamacException {
-        if (!SharedConceptsSecurityUtils.canCancelConceptSchemeValidity(getMetamacPrincipal(ctx), getType(conceptSchemeMetamacDto), getOperacionCode(conceptSchemeMetamacDto))) {
+    public static void canCancelConceptSchemeValidity(ServiceContext ctx, ConceptSchemeVersionMetamac conceptSchemeMetamac) throws MetamacException {
+        if (!SharedConceptsSecurityUtils.canCancelConceptSchemeValidity(getMetamacPrincipal(ctx), conceptSchemeMetamac.getType(), getOperacionCode(conceptSchemeMetamac))) {
             throw new MetamacException(ServiceExceptionType.SECURITY_ACTION_NOT_ALLOWED, ctx.getUserId());
         }
     }
@@ -120,17 +117,7 @@ public class ConceptsSecurityUtils {
     //
     // PRIVATE METHODS
     //
-
-    private static ItemSchemeMetamacProcStatusEnum getProcStatus(ConceptSchemeMetamacDto conceptSchemeMetamacDto) {
-        return conceptSchemeMetamacDto != null ? conceptSchemeMetamacDto.getProcStatus() : null;
+    private static String getOperacionCode(ConceptSchemeVersionMetamac conceptSchemeMetamac) {
+        return conceptSchemeMetamac.getRelatedOperation() != null ? conceptSchemeMetamac.getRelatedOperation().getCode() : null;
     }
-
-    private static ConceptSchemeTypeEnum getType(ConceptSchemeMetamacDto conceptSchemeMetamacDto) {
-        return conceptSchemeMetamacDto != null ? conceptSchemeMetamacDto.getType() : null;
-    }
-
-    private static String getOperacionCode(ConceptSchemeMetamacDto conceptSchemeMetamacDto) {
-        return conceptSchemeMetamacDto != null && conceptSchemeMetamacDto.getRelatedOperation() != null ? conceptSchemeMetamacDto.getRelatedOperation().getCode() : null;
-    }
-
 }
