4fc10caa66d9214e025f810ae636bbd7864159a2
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-09-18 09:07:22 +0000

    IN PROGRESS - issue METAMAC-1072: Conectar el versionado de DSD en la fachada de Metamac SRM http://jira.arte-consultores.com/browse/METAMAC-1072
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@40268 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/dsd/serviceapi/DsdsMetamacServiceTest.java
+++ metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/dsd/serviceapi/DsdsMetamacServiceTest.java
@@ -1,152 +1,216 @@
 package org.siemac.metamac.srm.core.dsd.serviceapi;
 
+import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.srm.core.common.SrmBaseTest;
+import org.siemac.metamac.srm.core.dsd.domain.DataStructureDefinitionVersionMetamac;
+import org.siemac.metamac.srm.core.dsd.serviceapi.utils.DataStructureDefinitionMetamacDoMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.arte.statistic.sdmx.srm.core.base.domain.Component;
+import com.arte.statistic.sdmx.srm.core.base.domain.ComponentList;
+import com.arte.statistic.sdmx.srm.core.structure.domain.DimensionComponent;
+import com.arte.statistic.sdmx.srm.core.structure.domain.GroupDimensionDescriptor;
+import com.arte.statistic.sdmx.srm.core.structure.serviceapi.DataStructureDefinitionService;
+import com.arte.statistic.sdmx.srm.core.structure.serviceapi.utils.DataStructureDefinitionDoMocks;
+import com.arte.statistic.sdmx.v2_1.domain.enume.srm.domain.TypeComponentList;
+import com.arte.statistic.sdmx.v2_1.domain.enume.srm.domain.VersionTypeEnum;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/srm/applicationContext-test.xml"})
 @TransactionConfiguration(transactionManager = "txManagerCore", defaultRollback = true)
 @Transactional
-public class DsdsMetamacServiceTest extends SrmBaseTest
-    implements DsdsMetamacServiceTestBase {
+public class DsdsMetamacServiceTest extends SrmBaseTest implements DsdsMetamacServiceTestBase {
+
     @Autowired
     protected DsdsMetamacService dsdsMetamacService;
+    
+    @Autowired
+    protected DataStructureDefinitionService dataStructureDefinitionService;
+    
+    private final ServiceContext serviceContext = new ServiceContext("system", "123456", "junit");
 
+    protected ServiceContext getServiceContext() {
+        return serviceContext;
+    }
+    
     @Test
     @Override
     public void testCreateDataStructureDefinition() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testUpdateDataStructureDefinition() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testRetrieveDataStructureDefinitionByUrn() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testRetrieveDataStructureDefinitionVersions() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testFindDataStructureDefinitionsByCondition() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testSaveDescriptorForDataStructureDefinition() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testDeleteDescriptorForDataStructureDefinition() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testSaveComponentForDataStructureDefinition() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testDeleteComponentForDataStructureDefinition() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testSendDataStructureDefinitionToProductionValidation() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testSendDataStructureDefinitionToDiffusionValidation() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testRejectDataStructureDefinitionProductionValidation() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testRejectDataStructureDefinitionDiffusionValidation() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testPublishInternallyDataStructureDefinition() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testPublishExternallyDataStructureDefinition() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testDeleteDataStructureDefinition() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Test
     @Override
     public void testVersioningDataStructureDefinition() throws Exception {
-        // TODO Auto-generated method stub
+        DataStructureDefinitionVersionMetamac dataStructureDefinitionVersionMetamacToCopy = createDataStructureDefinitionGraph();
         
+        dataStructureDefinitionService.markDataStructureAsFinal(getServiceContext(), dataStructureDefinitionVersionMetamacToCopy.getMaintainableArtefact().getUrn());
+        
+        DataStructureDefinitionVersionMetamac dataStructureDefinitionVersionMetamacCreated = dsdsMetamacService.versioningDataStructureDefinition(getServiceContextAdministrador(), dataStructureDefinitionVersionMetamacToCopy.getMaintainableArtefact().getUrn(), VersionTypeEnum.MAJOR);
+    
+        int kaka = 23;
     }
 
     @Test
     @Override
     public void testCancelDataStructureDefinitionValidity() throws Exception {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     protected String getDataSetFile() {
         return "dbunit/SrmDsdTest.xml";
     }
-   
+
+    private DataStructureDefinitionVersionMetamac createDataStructureDefinitionGraph() throws MetamacException {
+
+        // Create DSD
+        DataStructureDefinitionVersionMetamac dataStructureDefinitionVersionMetamac = dsdsMetamacService.createDataStructureDefinition(getServiceContext(), DataStructureDefinitionMetamacDoMocks.mockDataStructureDefinitionVersionMetamac());
+        dataStructureDefinitionVersionMetamac.getMaintainableArtefact().setIsCodeUpdated(Boolean.FALSE);
+
+        // Create Dimension Descriptor and components
+        Component measureDim = DataStructureDefinitionDoMocks.createMeasureDimension();
+        Component measureDimCreated = dsdsMetamacService.saveComponentForDataStructureDefinition(getServiceContext(), dataStructureDefinitionVersionMetamac.getMaintainableArtefact().getUrn(), measureDim,
+                TypeComponentList.DIMENSION_DESCRIPTOR);
+
+        Component dim = DataStructureDefinitionDoMocks.createDimension();
+        Component dimCreated = dsdsMetamacService.saveComponentForDataStructureDefinition(getServiceContext(), dataStructureDefinitionVersionMetamac.getMaintainableArtefact().getUrn(), dim,
+                TypeComponentList.DIMENSION_DESCRIPTOR);
+
+        Component timeDim = DataStructureDefinitionDoMocks.createTimeDimension();
+        Component timeDimCreated = dsdsMetamacService.saveComponentForDataStructureDefinition(getServiceContext(), dataStructureDefinitionVersionMetamac.getMaintainableArtefact().getUrn(), timeDim,
+                TypeComponentList.DIMENSION_DESCRIPTOR);
+
+        // Create GroupDimension Descriptor
+        ComponentList groupDescriptor = DataStructureDefinitionDoMocks.createGroupDimensionDescriptor((DimensionComponent) measureDimCreated);
+        ComponentList groupDescriptorCreated = dsdsMetamacService.saveDescriptorForDataStructureDefinition(getServiceContext(), dataStructureDefinitionVersionMetamac.getMaintainableArtefact().getUrn(),
+                groupDescriptor);
+
+        // Create Attribute Descriptor and components
+        Component dataAttribute = DataStructureDefinitionDoMocks.createDataAttribute((GroupDimensionDescriptor) groupDescriptorCreated, (DimensionComponent) dimCreated);
+        Component dataAttributeCreated = dsdsMetamacService.saveComponentForDataStructureDefinition(getServiceContext(), dataStructureDefinitionVersionMetamac.getMaintainableArtefact().getUrn(),
+                dataAttribute, TypeComponentList.ATTRIBUTE_DESCRIPTOR);
+
+        // Create Measure Descriptor and component
+        Component primaryMeasure = DataStructureDefinitionDoMocks.createPrimaryMeasure();
+        Component componentCreated = dsdsMetamacService.saveComponentForDataStructureDefinition(getServiceContext(), dataStructureDefinitionVersionMetamac.getMaintainableArtefact().getUrn(), primaryMeasure,
+                TypeComponentList.MEASURE_DESCRIPTOR);
+
+        return dataStructureDefinitionVersionMetamac;
+    }
+
 }
---
+++ metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/dsd/serviceapi/utils/DataStructureDefinitionMetamacDoMocks.java
@@ -0,0 +1,18 @@
+package org.siemac.metamac.srm.core.dsd.serviceapi.utils;
+
+import org.siemac.metamac.srm.core.dsd.domain.DataStructureDefinitionVersionMetamac;
+
+import com.arte.statistic.sdmx.srm.core.structure.serviceapi.utils.DataStructureDefinitionDoMocks;
+
+public class DataStructureDefinitionMetamacDoMocks extends DataStructureDefinitionDoMocks {
+
+    public static DataStructureDefinitionVersionMetamac mockDataStructureDefinitionVersionMetamac() {
+
+        DataStructureDefinitionVersionMetamac dataStructureDefinitionVersionMetamac = new DataStructureDefinitionVersionMetamac();
+        
+        mockDataStructureDefinition(dataStructureDefinitionVersionMetamac);
+        
+        return dataStructureDefinitionVersionMetamac;
+    }
+    
+}
