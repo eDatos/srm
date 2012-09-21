e15133788191d8469a96c8ed8917181d27d6654c
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-09-21 11:37:36 +0000

    METAMAC-1096 Entidades y DTOs de organizaciones (METAMAC)
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@40343 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/organisation/repositoryimpl/OrganisationSchemeVersionMetamacRepositoryImpl.java
@@ -0,0 +1,32 @@
+package org.siemac.metamac.srm.core.organisation.repositoryimpl;
+
+import org.siemac.metamac.srm.core.enume.domain.ItemSchemeMetamacProcStatusEnum;
+import org.siemac.metamac.srm.core.organisation.domain.OrganisationSchemeMetamac;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository implementation for OrganisationSchemeVersionMetamac
+ */
+@Repository("organisationSchemeVersionMetamacRepository")
+public class OrganisationSchemeVersionMetamacRepositoryImpl
+    extends OrganisationSchemeVersionMetamacRepositoryBase {
+    public OrganisationSchemeVersionMetamacRepositoryImpl() {
+    }
+
+    public OrganisationSchemeMetamac findByUrn(String urn) {
+
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("findByUrn not implemented");
+
+    }
+
+    public OrganisationSchemeMetamac retrieveOrganisationSchemeByProcStatus(
+        String urn, ItemSchemeMetamacProcStatusEnum[] procStatus) {
+
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+            "retrieveOrganisationSchemeByProcStatus not implemented");
+
+    }
+}
