18d11d79a6125ad84764589a3bb08ceb55a8bb49
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-09-27 07:03:46 +0000

    METAMAC-1105 Crear esquemas de organizaciones
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@40466 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/organisation/SaveOrganisationSchemeActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/organisation/SaveOrganisationSchemeActionHandler.java
@@ -1,8 +1,14 @@
 package org.siemac.metamac.srm.web.server.handlers.organisation;
 
+import org.siemac.metamac.core.common.exception.MetamacException;
+import org.siemac.metamac.srm.core.facade.serviceapi.SrmCoreServiceFacade;
+import org.siemac.metamac.srm.core.organisation.dto.OrganisationSchemeMetamacDto;
 import org.siemac.metamac.srm.web.shared.organisation.SaveOrganisationSchemeAction;
 import org.siemac.metamac.srm.web.shared.organisation.SaveOrganisationSchemeResult;
+import org.siemac.metamac.web.common.server.ServiceContextHolder;
 import org.siemac.metamac.web.common.server.handlers.SecurityActionHandler;
+import org.siemac.metamac.web.common.server.utils.WebExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.gwtplatform.dispatch.shared.ActionException;
@@ -10,8 +16,8 @@ import com.gwtplatform.dispatch.shared.ActionException;
 @Component
 public class SaveOrganisationSchemeActionHandler extends SecurityActionHandler<SaveOrganisationSchemeAction, SaveOrganisationSchemeResult> {
 
-    // @Autowired
-    // private SrmCoreServiceFacade srmCoreServiceFacade;
+    @Autowired
+    private SrmCoreServiceFacade srmCoreServiceFacade;
 
     public SaveOrganisationSchemeActionHandler() {
         super(SaveOrganisationSchemeAction.class);
@@ -19,7 +25,21 @@ public class SaveOrganisationSchemeActionHandler extends SecurityActionHandler<S
 
     @Override
     public SaveOrganisationSchemeResult executeSecurityAction(SaveOrganisationSchemeAction action) throws ActionException {
-        return new SaveOrganisationSchemeResult(action.getOrganisationSchemeToSave());
+        try {
+            OrganisationSchemeMetamacDto organisationSchemeToSave = action.getOrganisationSchemeToSave();
+            OrganisationSchemeMetamacDto savedOrganisationSchemeDto = null;
+            if (organisationSchemeToSave.getId() == null) {
+                // Create
+                savedOrganisationSchemeDto = srmCoreServiceFacade.createOrganisationScheme(ServiceContextHolder.getCurrentServiceContext(), organisationSchemeToSave);
+                return new SaveOrganisationSchemeResult(savedOrganisationSchemeDto);
+            } else {
+                // Update
+                // TODO savedOrganisationSchemeDto = srmCoreServiceFacade.updateOrganisationScheme(ServiceContextHolder.getCurrentServiceContext(), organisationSchemeToSave);
+            }
+            return new SaveOrganisationSchemeResult(savedOrganisationSchemeDto);
+        } catch (MetamacException e) {
+            throw WebExceptionUtils.createMetamacWebException(e);
+        }
     }
 
 }
