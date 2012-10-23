1e54208e7ecb24979697fd2186aadf0dbcaeb36e
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-10-23 07:58:10 +0000

    IN PROGRESS - issue METAMAC-1276: Importación de mensajes DSD. Adecuarla a la nueva estrategia de versionado del statistic-srm http://jira.arte-consultores.com/browse/METAMAC-1276
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@41226 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/dsd/mapper/DataStructureDefinitionDto2DoMapper.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/dsd/mapper/DataStructureDefinitionDto2DoMapper.java
@@ -8,12 +8,14 @@ import com.arte.statistic.sdmx.srm.core.base.domain.Component;
 import com.arte.statistic.sdmx.srm.core.base.domain.ComponentList;
 import com.arte.statistic.sdmx.v2_1.domain.dto.srm.ComponentDto;
 import com.arte.statistic.sdmx.v2_1.domain.dto.srm.ComponentListDto;
+import com.arte.statistic.sdmx.v2_1.domain.dto.srm.DataStructureDefinitionExtendDto;
 
 public interface DataStructureDefinitionDto2DoMapper {
 
     // TODO Cambiar a tipos de METAMAC
     public <U extends Component> U componentDtoToComponent(ComponentDto source) throws MetamacException;
-    public <U extends ComponentList> U componentListDtoToComponentList(ComponentListDto componentListDto) throws MetamacException;
+    public <U extends ComponentList> U componentListDtoToComponentList(ComponentListDto source) throws MetamacException;
 
-    public DataStructureDefinitionVersionMetamac dataStructureDefinitionDtoToDataStructureDefinition(DataStructureDefinitionMetamacDto dataStructureDefinitionMetamacDto) throws MetamacException;
+    public DataStructureDefinitionVersionMetamac dataStructureDefinitionDtoToDataStructureDefinition(DataStructureDefinitionMetamacDto source) throws MetamacException;
+    public DataStructureDefinitionVersionMetamac dataStructureDefinitionDtoToDataStructureDefinition(DataStructureDefinitionExtendDto source) throws MetamacException;
 }
