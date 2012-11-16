f2cd37de8c3819e822a2a81bdc56d023b1888dd1
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-11-16 08:29:27 +0000

    METAMAC-1368 Codelists y codes: entidades, estructura de servicio y test (metamac-srm)
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@41868 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/code/serviceimpl/CodesMetamacServiceImpl.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/code/serviceimpl/CodesMetamacServiceImpl.java
@@ -3,15 +3,10 @@ package org.siemac.metamac.srm.core.code.serviceimpl;
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.srm.core.code.domain.CodelistVersionMetamac;
-import org.siemac.metamac.srm.core.common.LifeCycle;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import com.arte.statistic.sdmx.srm.core.base.domain.ItemSchemeVersionRepository;
-import com.arte.statistic.sdmx.srm.core.code.domain.CodeRepository;
 import com.arte.statistic.sdmx.srm.core.code.serviceapi.CodesService;
-import com.arte.statistic.sdmx.srm.core.code.serviceimpl.utils.CodesDoCopyUtils.CodesCopyCallback;
 
 /**
  * Implementation of CodesMetamacService.
@@ -20,21 +15,21 @@ import com.arte.statistic.sdmx.srm.core.code.serviceimpl.utils.CodesDoCopyUtils.
 public class CodesMetamacServiceImpl extends CodesMetamacServiceImplBase {
 
     @Autowired
-    private CodesService                codesService;
+    private CodesService codesService;
 
-//    @Autowired
-//    private ItemSchemeVersionRepository itemSchemeVersionRepository;
-//
-//    @Autowired
-//    private CodeRepository              codeRepository;
-//
-//    @Autowired
-//    @Qualifier("codelistLifeCycle")
-//    private LifeCycle                   codelistLifeCycle;
-//
-//    @Autowired
-//    @Qualifier("codesCopyCallbackMetamac")
-//    private CodesCopyCallback           codesCopyCallback;
+    // @Autowired
+    // private ItemSchemeVersionRepository itemSchemeVersionRepository;
+    //
+    // @Autowired
+    // private CodeRepository codeRepository;
+    //
+    // @Autowired
+    // @Qualifier("codelistLifeCycle")
+    // private LifeCycle codelistLifeCycle;
+    //
+    // @Autowired
+    // @Qualifier("codesCopyCallbackMetamac")
+    // private CodesCopyCallback codesCopyCallback;
 
     public CodesMetamacServiceImpl() {
     }
