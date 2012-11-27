1cdbf022f9562b310a07a1d293d0c46ecb15754e
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-11-27 08:53:40 +0000

    METAMAC-1418 No guardar el endpoint de la api de operaciones estadísticas
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@42109 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/concept/GetStatisticalOperationsPaginatedListActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/concept/GetStatisticalOperationsPaginatedListActionHandler.java
@@ -3,10 +3,13 @@ package org.siemac.metamac.srm.web.server.handlers.concept;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+import org.siemac.metamac.core.common.conf.ConfigurationService;
 import org.siemac.metamac.core.common.dto.ExternalItemDto;
 import org.siemac.metamac.core.common.enume.domain.TypeExternalArtefactsEnum;
 import org.siemac.metamac.rest.common.v1_0.domain.Resource;
 import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.Operations;
+import org.siemac.metamac.srm.web.server.rest.RestApiConstants;
 import org.siemac.metamac.srm.web.server.rest.StatisticalOperationsRestInternalFacade;
 import org.siemac.metamac.srm.web.shared.concept.GetStatisticalOperationsPaginatedListAction;
 import org.siemac.metamac.srm.web.shared.concept.GetStatisticalOperationsPaginatedListResult;
@@ -23,12 +26,18 @@ public class GetStatisticalOperationsPaginatedListActionHandler extends Security
     @Autowired
     private StatisticalOperationsRestInternalFacade statisticalOperationsRestInternalFacade;
 
+    @Autowired
+    private ConfigurationService                    configurationService;
+
     public GetStatisticalOperationsPaginatedListActionHandler() {
         super(GetStatisticalOperationsPaginatedListAction.class);
     }
 
     @Override
     public GetStatisticalOperationsPaginatedListResult executeSecurityAction(GetStatisticalOperationsPaginatedListAction action) throws ActionException {
+
+        String operationsApiEndpoint = configurationService.getProperty(RestApiConstants.STATISTICAL_OPERATIONS_REST_INTERNAL);
+
         int firstResult = 0;
         int totalResults = 0;
         List<ExternalItemDto> externalItemDtos = new ArrayList<ExternalItemDto>();
@@ -37,8 +46,10 @@ public class GetStatisticalOperationsPaginatedListActionHandler extends Security
             firstResult = result.getOffset().intValue();
             totalResults = result.getTotal().intValue();
             for (Resource resource : result.getOperations()) {
-                // TODO NO GUARDAR EL ENDPOINT DE LA API
-                ExternalItemDto externalItemDto = new ExternalItemDto(resource.getId(), resource.getSelfLink(), resource.getUrn(), TypeExternalArtefactsEnum.STATISTICAL_OPERATION,
+                // Do not store rest api endpoint
+                String uri = StringUtils.removeStart(resource.getSelfLink().getHref(), operationsApiEndpoint);
+
+                ExternalItemDto externalItemDto = new ExternalItemDto(resource.getId(), uri, resource.getUrn(), TypeExternalArtefactsEnum.STATISTICAL_OPERATION,
                         DtoUtils.getInternationalStringDtoFromInternationalString(resource.getTitle()));
                 externalItemDtos.add(externalItemDto);
 
