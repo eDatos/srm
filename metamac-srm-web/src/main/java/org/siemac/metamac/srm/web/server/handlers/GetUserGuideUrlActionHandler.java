5ba21ce1d64cda0f03fe000685756f1d0e561d35
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-11-27 12:07:51 +0000

    METAMAC-1432 Separar directorio data para la aplicación interna y la externa
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@42141 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/GetUserGuideUrlActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/GetUserGuideUrlActionHandler.java
@@ -26,7 +26,7 @@ public class GetUserGuideUrlActionHandler extends SecurityActionHandler<GetUserG
     public GetUserGuideUrlResult executeSecurityAction(GetUserGuideUrlAction action) throws ActionException {
         String dataUrl = configurationService.getConfig().getString(PROP_DATA_URL);
         String userGuideFileName = configurationService.getConfig().getString(PROP_USER_GUIDE_FILE_NAME);
-        return new GetUserGuideUrlResult(dataUrl + "/srm/docs/" + userGuideFileName);
+        return new GetUserGuideUrlResult(dataUrl + "/srm/srm-web/docs/" + userGuideFileName);
     }
 
 }
