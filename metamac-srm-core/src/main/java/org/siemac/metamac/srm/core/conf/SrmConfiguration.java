994c3daa013c8e74e63adab12aaff4dbe45e6dd6
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-12-19 13:32:22 +0000

    Configuración: error si no encuentra propiedad de urn de mantainer
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@42560 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/conf/SrmConfiguration.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/conf/SrmConfiguration.java
@@ -1,9 +1,11 @@
 package org.siemac.metamac.srm.core.conf;
 
+import org.siemac.metamac.core.common.exception.MetamacException;
+
 public interface SrmConfiguration {
 
     /**
      * Retrieve organisation that is maintainer default to create artefacts
      */
-    public String retrieveMaintainerUrnDefault();
+    public String retrieveMaintainerUrnDefault() throws MetamacException;
 }
--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/conf/SrmConfigurationImpl.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/conf/SrmConfigurationImpl.java
@@ -1,6 +1,10 @@
 package org.siemac.metamac.srm.core.conf;
 
 import org.siemac.metamac.core.common.conf.ConfigurationService;
+import org.siemac.metamac.core.common.exception.MetamacException;
+import org.siemac.metamac.core.common.exception.MetamacExceptionBuilder;
+import org.siemac.metamac.core.common.util.shared.StringUtils;
+import org.siemac.metamac.srm.core.common.error.ServiceExceptionType;
 import org.siemac.metamac.srm.core.constants.SrmConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,7 +16,15 @@ public class SrmConfigurationImpl implements SrmConfiguration {
     private ConfigurationService configurationService;
 
     @Override
-    public String retrieveMaintainerUrnDefault() {
-        return configurationService.getProperty(SrmConstants.METAMAC_ORGANISATION_URN);
+    public String retrieveMaintainerUrnDefault() throws MetamacException {
+        return retrieveProperty(SrmConstants.METAMAC_ORGANISATION_URN, Boolean.TRUE);
+    }
+
+    private String retrieveProperty(String propertyName, Boolean required) throws MetamacException {
+        String propertyValue = configurationService.getProperty(propertyName);
+        if (StringUtils.isBlank(propertyValue)) {
+            throw MetamacExceptionBuilder.builder().withExceptionItems(ServiceExceptionType.CONFIGURATION_PROPERTY_NOT_FOUND).withMessageParameters(propertyName).build();
+        }
+        return propertyValue;
     }
 }
