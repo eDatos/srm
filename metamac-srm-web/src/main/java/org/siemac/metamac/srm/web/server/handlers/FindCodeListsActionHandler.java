b446cfd9b1288174e2191ba771b52c6cd8774acc
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-01-04 20:21:15 +0000

    Comentar llamadas erróneas a métodos en la fachada de servicios
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@42702 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/FindCodeListsActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/FindCodeListsActionHandler.java
@@ -1,15 +1,9 @@
 package org.siemac.metamac.srm.web.server.handlers;
 
-import java.util.List;
-
-import org.siemac.metamac.core.common.dto.ExternalItemDto;
-import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.srm.core.facade.serviceapi.SrmCoreServiceFacade;
 import org.siemac.metamac.srm.web.shared.FindCodeListsAction;
 import org.siemac.metamac.srm.web.shared.FindCodeListsResult;
-import org.siemac.metamac.web.common.server.ServiceContextHolder;
 import org.siemac.metamac.web.common.server.handlers.SecurityActionHandler;
-import org.siemac.metamac.web.common.server.utils.WebExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -28,13 +22,14 @@ public class FindCodeListsActionHandler extends SecurityActionHandler<FindCodeLi
 
     @Override
     public FindCodeListsResult executeSecurityAction(FindCodeListsAction action) throws ActionException {
-        try {
-            List<ExternalItemDto> codeLists = srmCoreServiceFacade.findCodelists(ServiceContextHolder.getCurrentServiceContext(), action.getUriConcept());
-            return new FindCodeListsResult(codeLists);
-        } catch (MetamacException e) {
-            throw WebExceptionUtils.createMetamacWebException(e);
-        }
-
+        // TODO
+        // try {
+        // List<ExternalItemDto> codeLists = srmCoreServiceFacade.findCodelists(ServiceContextHolder.getCurrentServiceContext(), action.getUriConcept());
+        // return new FindCodeListsResult(codeLists);
+        // } catch (MetamacException e) {
+        // throw WebExceptionUtils.createMetamacWebException(e);
+        // }
+        return null;
     }
 
     @Override
