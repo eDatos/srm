2b0a5ae5b37e81aba4f90dc64be7782ad6a60fe6
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-01-21 07:48:08 +0000

    METAMAC-1457: [CODES] Órdenes. Constante para orden alfabético
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@42960 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/constants/SrmConstants.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/constants/SrmConstants.java
@@ -4,8 +4,12 @@ import org.siemac.metamac.core.common.enume.domain.VersionPatternEnum;
 
 public class SrmConstants {
 
-    public static final String             SECURITY_APPLICATION_ID  = "GESTOR_RECURSOS_ESTRUCTURALES";
-    public static final VersionPatternEnum VERSION_PATTERN_METAMAC  = VersionPatternEnum.XX_YYY;
+    public static final String             SECURITY_APPLICATION_ID                        = "GESTOR_RECURSOS_ESTRUCTURALES";
 
-    public static final String             METAMAC_ORGANISATION_URN = "metamac.organisation.urn";
+    // Configuration
+    public static final String             METAMAC_ORGANISATION_URN                       = "metamac.organisation.urn";
+
+    // Artefacts
+    public static final VersionPatternEnum VERSION_PATTERN_METAMAC                        = VersionPatternEnum.XX_YYY;
+    public static final String             CODELIST_ORDER_VISUALISATION_ALPHABETICAL_CODE = "ALPHABETICAL";
 }
