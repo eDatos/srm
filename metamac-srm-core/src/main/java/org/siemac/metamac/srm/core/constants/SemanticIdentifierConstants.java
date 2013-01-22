251d6c5102a4bf6cc0e2af89b67d6d44a9365f77
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-01-22 08:02:48 +0000

    METAMAC-1459 Corregido nombre de expresiones regulares con identificadores semánticos
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43026 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/constants/SemanticIdentifierConstants.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/constants/SemanticIdentifierConstants.java
@@ -6,9 +6,9 @@ public class SemanticIdentifierConstants extends com.arte.statistic.sdmx.srm.cor
 
     // Note: only for web
 
-    public static final String CODELIST_FAMILY_CODE_PATTERN                     = SDMXCommonRegExpV2_1.METAMAC_ID;
-    public static final String VARIABLE_FAMILY_CODE_PATTERN                     = SDMXCommonRegExpV2_1.METAMAC_ID;
-    public static final String VARIABLE_CODE_PATTERN                            = SDMXCommonRegExpV2_1.METAMAC_ID;
-    public static final String VARIABLE_ELEMENT_FAMILY_CODE_PATTERN             = SDMXCommonRegExpV2_1.METAMAC_ID;
-    public static final String CODELIST_ORDER_VISUALISATION_FAMILY_CODE_PATTERN = SDMXCommonRegExpV2_1.METAMAC_ID;
+    public static final String CODELIST_FAMILY_CODE_PATTERN              = SDMXCommonRegExpV2_1.METAMAC_ID;
+    public static final String VARIABLE_FAMILY_CODE_PATTERN              = SDMXCommonRegExpV2_1.METAMAC_ID;
+    public static final String VARIABLE_CODE_PATTERN                     = SDMXCommonRegExpV2_1.METAMAC_ID;
+    public static final String VARIABLE_ELEMENT_CODE_PATTERN             = SDMXCommonRegExpV2_1.METAMAC_ID;
+    public static final String CODELIST_ORDER_VISUALISATION_CODE_PATTERN = SDMXCommonRegExpV2_1.METAMAC_ID;
 }
