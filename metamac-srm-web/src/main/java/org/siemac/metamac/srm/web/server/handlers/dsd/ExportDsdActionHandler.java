38f7f7e3a30adc95adb0076a8c79d60e0d96101d
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-01-25 07:19:57 +0000

    Comentada llama a método "exportSDMXStructureMsg" por cambios en la facade
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43108 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/dsd/ExportDsdActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/dsd/ExportDsdActionHandler.java
@@ -1,19 +1,12 @@
 package org.siemac.metamac.srm.web.server.handlers.dsd;
 
-import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.srm.core.facade.serviceapi.SrmCoreServiceFacade;
 import org.siemac.metamac.srm.web.shared.dsd.ExportDsdAction;
 import org.siemac.metamac.srm.web.shared.dsd.ExportDsdResult;
-import org.siemac.metamac.web.common.server.ServiceContextHolder;
 import org.siemac.metamac.web.common.server.handlers.SecurityActionHandler;
-import org.siemac.metamac.web.common.server.utils.WebExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.arte.statistic.sdmx.v2_1.domain.dto.srm.DataStructureDefinitionExtendDto;
-import com.arte.statistic.sdmx.v2_1.domain.dto.trans.StructureMsgDto;
-import com.arte.statistic.sdmx.v2_1.domain.enume.srm.domain.TypeDozerCopyMode;
-import com.gwtplatform.dispatch.server.ExecutionContext;
 import com.gwtplatform.dispatch.shared.ActionException;
 
 @Component
@@ -28,22 +21,17 @@ public class ExportDsdActionHandler extends SecurityActionHandler<ExportDsdActio
 
     @Override
     public ExportDsdResult executeSecurityAction(ExportDsdAction action) throws ActionException {
-        StructureMsgDto structureMsgDto = new StructureMsgDto();
+        // StructureMsgDto structureMsgDto = new StructureMsgDto();
 
-        try {
-            DataStructureDefinitionExtendDto dataStructureDefinitionExtendDto = srmCoreServiceFacade.retrieveExtendedDataStructureDefinition(ServiceContextHolder.getCurrentServiceContext(),
-                    action.getUrn(), TypeDozerCopyMode.COPY_ALL_METADATA);
-            structureMsgDto.getDataStructureDefinitionDtos().add(dataStructureDefinitionExtendDto);
-            String fileName = srmCoreServiceFacade.exportSDMXStructureMsg(ServiceContextHolder.getCurrentServiceContext(), structureMsgDto);
-            return new ExportDsdResult(fileName);
-        } catch (MetamacException e) {
-            throw WebExceptionUtils.createMetamacWebException(e);
-        }
+        // try {
+        // TODO
+        // DataStructureDefinitionExtendDto dataStructureDefinitionExtendDto = srmCoreServiceFacade.retrieveExtendedDataStructureDefinition(ServiceContextHolder.getCurrentServiceContext(),
+        // action.getUrn(), TypeDozerCopyMode.COPY_ALL_METADATA);
+        // structureMsgDto.getDataStructureDefinitionDtos().add(dataStructureDefinitionExtendDto);
+        // String fileName = srmCoreServiceFacade.exportSDMXStructureMsg(ServiceContextHolder.getCurrentServiceContext(), structureMsgDto);
+        return new ExportDsdResult(null);
+        // } catch (MetamacException e) {
+        // throw WebExceptionUtils.createMetamacWebException(e);
+        // }
     }
-
-    @Override
-    public void undo(ExportDsdAction action, ExportDsdResult result, ExecutionContext context) throws ActionException {
-
-    }
-
 }
