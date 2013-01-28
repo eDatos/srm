8a40e38248ffae77fcc13dd41947fd64e56ecc52
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-01-28 13:34:14 +0000

    METAMAC-1462 Metadatos de visualización de DSDs: showDecimalsPrecision
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43141 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/dsd/widgets/ShowDecimalsPrecisionItem.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/dsd/widgets/ShowDecimalsPrecisionItem.java
@@ -1,12 +1,17 @@
 package org.siemac.metamac.srm.web.dsd.widgets;
 
+import static org.siemac.metamac.srm.web.client.MetamacSrmWeb.getConstants;
+
 import java.util.List;
 
 import org.siemac.metamac.srm.core.concept.dto.ConceptSchemeMetamacDto;
 import org.siemac.metamac.srm.core.dsd.dto.MeasureDimensionPrecisionDto;
+import org.siemac.metamac.web.common.client.resources.GlobalResources;
 import org.siemac.metamac.web.common.client.widgets.form.fields.CustomCanvasItem;
 
 import com.arte.statistic.sdmx.v2_1.domain.dto.srm.ItemHierarchyDto;
+import com.smartgwt.client.types.VerticalAlignment;
+import com.smartgwt.client.widgets.form.fields.FormItemIcon;
 
 public class ShowDecimalsPrecisionItem extends CustomCanvasItem {
 
@@ -18,6 +23,18 @@ public class ShowDecimalsPrecisionItem extends CustomCanvasItem {
 
         measureDimensionConceptsTreeGrid = new MeasureDimensionConceptsTreeGrid(editionMode);
         setCanvas(measureDimensionConceptsTreeGrid);
+
+        if (editionMode) {
+
+            // In edition mode, show a message to warn the user that these values should be filled when the measure dimension is not going to be modified
+
+            FormItemIcon infoIcon = new FormItemIcon();
+            infoIcon.setSrc(GlobalResources.RESOURCE.info().getURL());
+            infoIcon.setPrompt(getConstants().dsdShowDecimalsPrecisionInfo());
+            setIcons(infoIcon);
+
+            setIconVAlign(VerticalAlignment.TOP);
+        }
     }
 
     public void setMeasureDimensionPrecisions(List<MeasureDimensionPrecisionDto> measureDimensionPrecisionDtos) {
