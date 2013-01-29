fd71778ebe004d395a0839637b31115b571df113
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-01-29 11:42:52 +0000

    Refactor: CustomToolStripButton por RadioToolStripButton
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43171 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/widgets/StructuralResourcesMenu.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/widgets/StructuralResourcesMenu.java
@@ -2,7 +2,7 @@ package org.siemac.metamac.srm.web.client.widgets;
 
 import org.siemac.metamac.srm.web.client.MetamacSrmWeb;
 import org.siemac.metamac.srm.web.client.enums.ToolStripButtonEnum;
-import org.siemac.metamac.web.common.client.widgets.CustomToolStripButton;
+import org.siemac.metamac.web.common.client.widgets.RadioToolStripButton;
 
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.widgets.events.HasClickHandlers;
@@ -10,30 +10,30 @@ import com.smartgwt.client.widgets.toolbar.ToolStrip;
 
 public class StructuralResourcesMenu extends ToolStrip {
 
-    private CustomToolStripButton dsdListButton;
-    private CustomToolStripButton conceptsButton;
-    private CustomToolStripButton organisationsButton;
-    private CustomToolStripButton categoriesButton;
-    private CustomToolStripButton codelistsButton;
+    private RadioToolStripButton dsdListButton;
+    private RadioToolStripButton conceptsButton;
+    private RadioToolStripButton organisationsButton;
+    private RadioToolStripButton categoriesButton;
+    private RadioToolStripButton codelistsButton;
 
     public StructuralResourcesMenu() {
         super();
         setWidth100();
         setAlign(Alignment.LEFT);
 
-        conceptsButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().concepts());
+        conceptsButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().concepts());
         conceptsButton.setID(ToolStripButtonEnum.CONCEPTS.getValue());
 
-        codelistsButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().codelists());
+        codelistsButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().codelists());
         codelistsButton.setID(ToolStripButtonEnum.CODELISTS.getValue());
 
-        dsdListButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().dsds());
+        dsdListButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().dsds());
         dsdListButton.setID(ToolStripButtonEnum.DSD_LIST.getValue());
 
-        organisationsButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().organisations());
+        organisationsButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().organisations());
         organisationsButton.setID(ToolStripButtonEnum.ORGANISATIONS.getValue());
 
-        categoriesButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().categories());
+        categoriesButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().categories());
         categoriesButton.setID(ToolStripButtonEnum.CATEGORIES.getValue());
 
         addButton(conceptsButton);
--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/widgets/view/ToolStripViewImpl.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/widgets/view/ToolStripViewImpl.java
@@ -3,7 +3,7 @@ package org.siemac.metamac.srm.web.client.widgets.view;
 import org.siemac.metamac.srm.web.client.MetamacSrmWeb;
 import org.siemac.metamac.srm.web.client.enums.ToolStripButtonEnum;
 import org.siemac.metamac.srm.web.client.widgets.presenter.ToolStripPresenterWidget;
-import org.siemac.metamac.web.common.client.widgets.CustomToolStripButton;
+import org.siemac.metamac.web.common.client.widgets.RadioToolStripButton;
 
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
@@ -14,14 +14,14 @@ import com.smartgwt.client.widgets.toolbar.ToolStrip;
 @Deprecated
 public class ToolStripViewImpl implements ToolStripPresenterWidget.ToolStripView {
 
-    private ToolStrip             toolStrip;
+    private ToolStrip            toolStrip;
 
-    private CustomToolStripButton dsdListButton;
-    private CustomToolStripButton conceptsButton;
-    private CustomToolStripButton organisationsButton;
-    private CustomToolStripButton classificationsButton;
-    private CustomToolStripButton categoriesButton;
-    private CustomToolStripButton codelistsButton;
+    private RadioToolStripButton dsdListButton;
+    private RadioToolStripButton conceptsButton;
+    private RadioToolStripButton organisationsButton;
+    private RadioToolStripButton classificationsButton;
+    private RadioToolStripButton categoriesButton;
+    private RadioToolStripButton codelistsButton;
 
     @Inject
     public ToolStripViewImpl() {
@@ -30,22 +30,22 @@ public class ToolStripViewImpl implements ToolStripPresenterWidget.ToolStripView
         toolStrip.setWidth100();
         toolStrip.setAlign(Alignment.LEFT);
 
-        conceptsButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().concepts());
+        conceptsButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().concepts());
         conceptsButton.setID(ToolStripButtonEnum.CONCEPTS.getValue());
 
-        classificationsButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().codelists());
+        classificationsButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().codelists());
         classificationsButton.setID(ToolStripButtonEnum.CODELISTS.getValue());
 
-        dsdListButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().dsds());
+        dsdListButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().dsds());
         dsdListButton.setID(ToolStripButtonEnum.DSD_LIST.getValue());
 
-        organisationsButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().organisations());
+        organisationsButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().organisations());
         organisationsButton.setID(ToolStripButtonEnum.ORGANISATIONS.getValue());
 
-        categoriesButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().categories());
+        categoriesButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().categories());
         categoriesButton.setID(ToolStripButtonEnum.CATEGORIES.getValue());
 
-        codelistsButton = new CustomToolStripButton(MetamacSrmWeb.getConstants().codelists());
+        codelistsButton = new RadioToolStripButton(MetamacSrmWeb.getConstants().codelists());
         codelistsButton.setID(ToolStripButtonEnum.CODELISTS.getValue());
 
         toolStrip.addButton(conceptsButton);
