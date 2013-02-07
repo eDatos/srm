3c8cfc6d4df1216c7432c718cb7ee7a8cf6b381a
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-07 13:42:57 +0000

    METAMAC-1208 [CATEGORÍAS] Categorizaciones: crear CategoryWebCriteria
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43516 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/shared/criteria/CategoryWebCriteria.java
@@ -0,0 +1,37 @@
+package org.siemac.metamac.srm.web.shared.criteria;
+
+public class CategoryWebCriteria extends MetamacWebCriteria {
+
+    private static final long serialVersionUID = 1L;
+
+    private String            categorySchemeUrn;
+    private Boolean           isExternallyPublished;
+
+    public CategoryWebCriteria() {
+    }
+
+    public CategoryWebCriteria(String criteria) {
+        super(criteria);
+    }
+
+    public CategoryWebCriteria(String criteria, String categorySchemeUrn) {
+        super(criteria);
+        this.categorySchemeUrn = categorySchemeUrn;
+    }
+
+    public String getCategorySchemeUrn() {
+        return categorySchemeUrn;
+    }
+
+    public void setCategorySchemeUrn(String categorySchemeUrn) {
+        this.categorySchemeUrn = categorySchemeUrn;
+    }
+
+    public Boolean getIsExternallyPublished() {
+        return isExternallyPublished;
+    }
+
+    public void setExternallyPublished(Boolean isExternallyPublished) {
+        this.isExternallyPublished = isExternallyPublished;
+    }
+}
