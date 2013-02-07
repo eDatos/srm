bc4963b2da57f583add5ab0611716f60faa239a3
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-07 13:37:43 +0000

    METAMAC-1208 [CATEGORÍAS] Categorizaciones: añadir procStatus a CategorySchemeWebCriteria
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43515 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/shared/criteria/CategorySchemeWebCriteria.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/shared/criteria/CategorySchemeWebCriteria.java
@@ -1,10 +1,13 @@
 package org.siemac.metamac.srm.web.shared.criteria;
 
+import org.siemac.metamac.srm.core.enume.domain.ProcStatusEnum;
+
 public class CategorySchemeWebCriteria extends MetamacWebCriteria {
 
     private static final long serialVersionUID = 1L;
 
     private Boolean           isLastVersion;
+    private ProcStatusEnum    procStatus;
 
     public CategorySchemeWebCriteria() {
     }
@@ -19,5 +22,13 @@ public class CategorySchemeWebCriteria extends MetamacWebCriteria {
 
     public void setIsLastVersion(Boolean isLastVersion) {
         this.isLastVersion = isLastVersion;
+    }
+
+    public ProcStatusEnum getProcStatus() {
+        return procStatus;
+    }
+
+    public void setProcStatus(ProcStatusEnum procStatus) {
+        this.procStatus = procStatus;
     }
 }
