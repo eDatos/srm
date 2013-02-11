3ee9281aeb5d76c2cabe6a29cd390c477e3808b1
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-11 08:51:27 +0000

    METAMAC-1517 Gestión de mensajes de error con parámetros Seriazables de múlitples tipos
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43573 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/utils/ErrorUtils.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/utils/ErrorUtils.java
@@ -1,81 +1,13 @@
 package org.siemac.metamac.srm.web.client.utils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.siemac.metamac.srm.web.client.MetamacSrmWeb;
 import org.siemac.metamac.web.common.client.utils.CommonErrorUtils;
-import org.siemac.metamac.web.common.client.utils.CommonWebUtils;
-import org.siemac.metamac.web.common.shared.constants.CommonSharedConstants;
-import org.siemac.metamac.web.common.shared.exception.MetamacWebException;
-import org.siemac.metamac.web.common.shared.exception.MetamacWebExceptionItem;
 
 public class ErrorUtils extends CommonErrorUtils {
 
     public static List<String> getErrorMessages(Throwable caught, String alternativeMessage) {
-        List<String> list = new ArrayList<String>();
-        if (caught != null && caught instanceof MetamacWebException) {
-            List<MetamacWebExceptionItem> metamacExceptionItems = ((MetamacWebException) caught).getWebExceptionItems();
-            if (metamacExceptionItems.isEmpty()) {
-                list.add(alternativeMessage);
-            } else {
-                for (MetamacWebExceptionItem item : metamacExceptionItems) {
-                    if (CommonSharedConstants.EXCEPTION_UNKNOWN.equals(item.getCode())) {
-                        // If exception code is EXCEPTION_UNKNOWN, show alternative message
-                        list.add(alternativeMessage);
-                    } else {
-                        // If there is any problem getting message from code, show alternative message
-                        try {
-                            list.add(getMessage(item));
-                        } catch (MetamacWebException e) {
-                            list.add(alternativeMessage);
-                        }
-                    }
-                }
-            }
-        } else {
-            list.add(alternativeMessage);
-        }
-        return list;
-    }
-
-    private static String getMessage(MetamacWebExceptionItem item) throws MetamacWebException {
-        if (item != null && item.getCode() != null) {
-            // GWT generate a "_" separated method when the key is separated by "."
-            String code = transformMessageCode(item.getCode());
-            try {
-                String message = MetamacSrmWeb.getCoreMessages().getString(code);
-                return getMessageWithParameters(message, getTranslatedParameters(item.getMessageParameters()));
-            } catch (Exception e) {
-                throw new MetamacWebException("MESSAGE_NOT_FOUND", "Message with code = " + code + " not found");
-            }
-        }
-        throw new MetamacWebException("NULL_MESSAGE", "Message with null code");
-    }
-
-    private static String[] getTranslatedParameters(List<String[]> messageParameters) {
-        if (messageParameters != null) {
-            String[] translatedParameters = new String[messageParameters.size()];
-            for (int i = 0; i < messageParameters.size(); i++) {
-                List<String> params = new ArrayList<String>();
-                for (String param : messageParameters.get(i)) {
-                    params.add(getTranslatedParameter(param));
-                }
-                String translatedParameter = CommonWebUtils.getStringListToString(params);
-                translatedParameters[i] = translatedParameter;
-            }
-            return translatedParameters;
-        }
-        return null;
-    }
-
-    private static String getTranslatedParameter(String parameter) {
-        String code = transformMessageCode(String.valueOf(parameter));
-        try {
-            // If parameter is translated, return the translation
-            return MetamacSrmWeb.getCoreMessages().getString(code) + " (" + String.valueOf(parameter) + ")";
-        } catch (Exception e) {
-            return String.valueOf(parameter);
-        }
+        return getErrorMessages(MetamacSrmWeb.getCoreMessages(), caught, alternativeMessage);
     }
 }
