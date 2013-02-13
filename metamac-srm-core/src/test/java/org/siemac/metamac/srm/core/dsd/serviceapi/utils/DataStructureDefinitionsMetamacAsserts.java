8e8f892b5129bbaa5b70b1dbfcfce9ec31590d89
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-13 10:00:02 +0000

    IN PROGRESS - issue METAMAC-1482: [DSD] Asociar DSD con operación estadística http://jira.arte-consultores.com/browse/METAMAC-1482
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43644 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/dsd/serviceapi/utils/DataStructureDefinitionsMetamacAsserts.java
+++ metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/dsd/serviceapi/utils/DataStructureDefinitionsMetamacAsserts.java
@@ -10,15 +10,22 @@ import org.siemac.metamac.srm.core.dsd.domain.MeasureDimensionPrecision;
 import org.siemac.metamac.srm.core.dsd.dto.DataStructureDefinitionMetamacDto;
 import org.siemac.metamac.srm.core.dsd.dto.MeasureDimensionPrecisionDto;
 
-import com.arte.statistic.sdmx.srm.core.structure.domain.DataStructureDefinitionVersion;
 import com.arte.statistic.sdmx.srm.core.structure.serviceapi.utils.DataStructureDefinitionAsserts;
 import com.arte.statistic.sdmx.v2_1.domain.dto.common.RelatedResourceDto;
 
 public class DataStructureDefinitionsMetamacAsserts extends DataStructureDefinitionAsserts {
 
-    public static void assertEqualsDataStructureDefinition(DataStructureDefinitionVersion expected, DataStructureDefinitionVersion actual) {
+    public static void assertEqualsDataStructureDefinition(DataStructureDefinitionVersionMetamac expected, DataStructureDefinitionVersionMetamac actual) {
 
         // Metamac
+        assertEquals(expected.getAutoOpen(), actual.getAutoOpen());
+        assertEquals(expected.getShowDecimals(), actual.getShowDecimals());
+        assertEqualsExternalItem(expected.getStatisticalOperation(), actual.getStatisticalOperation());
+
+        assertDimensionOrdersEntity(expected.getHeadingDimensions(), actual.getHeadingDimensions());
+        assertDimensionOrdersEntity(expected.getStubDimensions(), actual.getStubDimensions());
+
+        assertDecimalPrecisionsEntity(expected.getShowDecimalsPrecisions(), actual.getShowDecimalsPrecisions());
 
         // Sdmx
         DataStructureDefinitionAsserts.assertEqualsDataStructureDefinition(expected, actual);
@@ -29,6 +36,7 @@ public class DataStructureDefinitionsMetamacAsserts extends DataStructureDefinit
         // Metamac
         assertEquals(expected.getAutoOpen(), actual.getAutoOpen());
         assertEquals(expected.getShowDecimals(), actual.getShowDecimals());
+        assertEqualsExternalItem(expected.getStatisticalOperation(), actual.getStatisticalOperation());
 
         assertDimensionOrders(expected.getHeadingDimensions(), actual.getHeadingDimensions());
         assertDimensionOrders(expected.getStubDimensions(), actual.getStubDimensions());
@@ -39,6 +47,7 @@ public class DataStructureDefinitionsMetamacAsserts extends DataStructureDefinit
         // DataStructureDefinitionAsserts.assertEqualsDataStructureDefinition(expected, actual);
 
     }
+
     private static void assertDimensionOrders(List<RelatedResourceDto> expected, List<DimensionOrder> actual) {
         for (int i = 0; i < expected.size(); i++) {
             assertDimensionOrder(expected.get(i), actual.get(i), i + 1);
@@ -58,6 +67,28 @@ public class DataStructureDefinitionsMetamacAsserts extends DataStructureDefinit
 
     private static void assertDecimalPrecision(MeasureDimensionPrecisionDto expected, MeasureDimensionPrecision actual, Integer i) {
         assertEquals(expected.getUrn(), actual.getConcept().getNameableArtefact().getUrn());
+        assertEquals(expected.getShowDecimalPrecision(), actual.getShowDecimalPrecision());
+    }
+
+    private static void assertDimensionOrdersEntity(List<DimensionOrder> expected, List<DimensionOrder> actual) {
+        for (int i = 0; i < expected.size(); i++) {
+            assertDimensionOrder(expected.get(i), actual.get(i), i + 1);
+        }
+    }
+
+    private static void assertDimensionOrder(DimensionOrder expected, DimensionOrder actual, Integer i) {
+        assertEquals(expected.getDimension().getUrn(), actual.getDimension().getUrn());
+        assertEquals(i, actual.getDimOrder());
+    }
+
+    private static void assertDecimalPrecisionsEntity(List<MeasureDimensionPrecision> expected, List<MeasureDimensionPrecision> actual) {
+        for (int i = 0; i < expected.size(); i++) {
+            assertDecimalPrecision(expected.get(i), actual.get(i), i + 1);
+        }
+    }
+
+    private static void assertDecimalPrecision(MeasureDimensionPrecision expected, MeasureDimensionPrecision actual, Integer i) {
+        assertEquals(expected.getConcept().getNameableArtefact().getUrn(), actual.getConcept().getNameableArtefact().getUrn());
         assertEquals(expected.getShowDecimalPrecision(), actual.getShowDecimalPrecision());
     }
 }
