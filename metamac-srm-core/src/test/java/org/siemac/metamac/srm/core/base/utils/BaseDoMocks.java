91d54f413014b613c09b6cdc63af7148d354e0ca
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-13 09:10:58 +0000

    DoMocks con valores fijados
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43634 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/base/utils/BaseDoMocks.java
+++ metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/base/utils/BaseDoMocks.java
@@ -3,6 +3,9 @@ package org.siemac.metamac.srm.core.base.utils;
 import org.joda.time.DateTime;
 import org.siemac.metamac.srm.core.base.domain.SrmLifeCycleMetadata;
 import org.siemac.metamac.srm.core.enume.domain.ProcStatusEnum;
+import org.siemac.metamac.srm.core.organisation.domain.OrganisationMetamac;
+
+import com.arte.statistic.sdmx.srm.core.base.domain.NameableArtefact;
 
 public class BaseDoMocks extends com.arte.statistic.sdmx.srm.core.base.serviceapi.utils.BaseDoMocks {
 
@@ -19,4 +22,26 @@ public class BaseDoMocks extends com.arte.statistic.sdmx.srm.core.base.serviceap
         return lifeCycleMetadata;
     }
 
+    public static SrmLifeCycleMetadata mockLifeCycleExternallyPublished() {
+        SrmLifeCycleMetadata lifeCycleMetadata = new SrmLifeCycleMetadata(ProcStatusEnum.EXTERNALLY_PUBLISHED);
+        lifeCycleMetadata.setProductionValidationDate(new DateTime(2009, 9, 1, 1, 1, 1, 1));
+        lifeCycleMetadata.setProductionValidationUser("production-user");
+        lifeCycleMetadata.setDiffusionValidationDate(new DateTime(2010, 10, 2, 1, 1, 1, 1));
+        lifeCycleMetadata.setDiffusionValidationUser("diffusion-user");
+        lifeCycleMetadata.setInternalPublicationDate(new DateTime(2011, 11, 3, 1, 1, 1, 1));
+        lifeCycleMetadata.setInternalPublicationUser("internal-publication-user");
+        lifeCycleMetadata.setExternalPublicationDate(new DateTime(2012, 12, 4, 1, 1, 1, 1));
+        lifeCycleMetadata.setExternalPublicationUser("external-publication-user");
+        lifeCycleMetadata.setIsExternalPublicationFailed(Boolean.FALSE);
+        lifeCycleMetadata.setExternalPublicationFailedDate(new DateTime(2013, 8, 2, 1, 1, 1, 1));
+        return lifeCycleMetadata;
+    }
+
+    public static OrganisationMetamac mockMaintainer(String agencyID) {
+        OrganisationMetamac target = new OrganisationMetamac();
+        target.setIdAsMaintainer("idAsMaintainer" + agencyID);
+        target.setNameableArtefact(new NameableArtefact());
+        mockNameableArtefactFixedValues(target.getNameableArtefact(), agencyID, null);
+        return target;
+    }
 }
