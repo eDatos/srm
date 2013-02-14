6beee6154fc9f3725fdf198c2006603fee2a38ef
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-14 13:43:31 +0000

    METAMAC-867 Seguridad en DSDs
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43692 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/dsd/utils/DsdClientSecurityUtils.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/dsd/utils/DsdClientSecurityUtils.java
@@ -1,73 +1,86 @@
 package org.siemac.metamac.srm.web.dsd.utils;
 
 import org.siemac.metamac.srm.core.enume.domain.ProcStatusEnum;
-import org.siemac.metamac.srm.core.security.shared.SharedDsdSecurityUtils;
-import org.siemac.metamac.srm.web.client.MetamacSrmWeb;
 
 public class DsdClientSecurityUtils {
 
     public static boolean canCreateDsd() {
-        return SharedDsdSecurityUtils.canCreateDataStructureDefinition(MetamacSrmWeb.getCurrentUser());
+        // return SharedDsdSecurityUtils.canCreateDataStructureDefinition(MetamacSrmWeb.getCurrentUser());
+        return true;
     }
 
     public static boolean canUpdateDsd(ProcStatusEnum procStatus) {
-        return SharedDsdSecurityUtils.canUpdateDsd(MetamacSrmWeb.getCurrentUser(), procStatus);
+        // return SharedDsdSecurityUtils.canUpdateDsd(MetamacSrmWeb.getCurrentUser(), procStatus);
+        return true;
     }
 
     public static boolean canVersioningDsd() {
-        return SharedDsdSecurityUtils.canVersioningDsd(MetamacSrmWeb.getCurrentUser());
+        // return SharedDsdSecurityUtils.canVersioningDsd(MetamacSrmWeb.getCurrentUser());
+        return true;
     }
 
     public static boolean canUpdatePrimaryMeasure(ProcStatusEnum procStatus) {
-        return SharedDsdSecurityUtils.canUpdatePrimaryMeasure(MetamacSrmWeb.getCurrentUser(), procStatus);
+        return true;
+        // return SharedDsdSecurityUtils.canUpdatePrimaryMeasure(MetamacSrmWeb.getCurrentUser(), procStatus);
     }
 
     public static boolean canUpdateDimensions(ProcStatusEnum procStatus) {
-        return SharedDsdSecurityUtils.canUpdateDimensions(MetamacSrmWeb.getCurrentUser(), procStatus);
+        return true;
+        // return SharedDsdSecurityUtils.canUpdateDimensions(MetamacSrmWeb.getCurrentUser(), procStatus);
     }
 
     public static boolean canUpdateAttributes(ProcStatusEnum procStatus) {
-        return SharedDsdSecurityUtils.canUpdateAttributes(MetamacSrmWeb.getCurrentUser(), procStatus);
+        return true;
+        // return SharedDsdSecurityUtils.canUpdateAttributes(MetamacSrmWeb.getCurrentUser(), procStatus);
     }
 
     public static boolean canUpdateGroupKeys(ProcStatusEnum procStatus) {
-        return SharedDsdSecurityUtils.canUpdateGroupKeys(MetamacSrmWeb.getCurrentUser(), procStatus);
+        return true;
+        // return SharedDsdSecurityUtils.canUpdateGroupKeys(MetamacSrmWeb.getCurrentUser(), procStatus);
     }
 
     public static boolean canDeleteDsd(ProcStatusEnum procStatus) {
-        return SharedDsdSecurityUtils.canDeleteDsd(MetamacSrmWeb.getCurrentUser(), procStatus);
+        return true;
+        // return SharedDsdSecurityUtils.canDeleteDsd(MetamacSrmWeb.getCurrentUser(), procStatus);
     }
 
     public static boolean canImportDsd() {
-        return SharedDsdSecurityUtils.canImportDsd(MetamacSrmWeb.getCurrentUser());
+        return true;
+        // return SharedDsdSecurityUtils.canImportDsd(MetamacSrmWeb.getCurrentUser());
     }
 
     public static boolean canCancelDsdValidity() {
-        return SharedDsdSecurityUtils.canEndDsdValidity(MetamacSrmWeb.getCurrentUser());
+        return true;
+        // return SharedDsdSecurityUtils.canEndDsdValidity(MetamacSrmWeb.getCurrentUser());
     }
 
     public static boolean canAnnounceDsd() {
-        return SharedDsdSecurityUtils.canAnnounceDsd(MetamacSrmWeb.getCurrentUser());
+        return true;
+        // return SharedDsdSecurityUtils.canAnnounceDsd(MetamacSrmWeb.getCurrentUser());
     }
 
     public static boolean canSendDsdToProductionValidation() {
-        return SharedDsdSecurityUtils.canSendDsdToProductionValidation(MetamacSrmWeb.getCurrentUser());
+        return true;
+        // return SharedDsdSecurityUtils.canSendDsdToProductionValidation(MetamacSrmWeb.getCurrentUser());
     }
 
     public static boolean canSendDsdToDiffusionValidation() {
-        return SharedDsdSecurityUtils.canSendDsdToDiffusionValidation(MetamacSrmWeb.getCurrentUser());
+        return true;
+        // return SharedDsdSecurityUtils.canSendDsdToDiffusionValidation(MetamacSrmWeb.getCurrentUser());
     }
 
     public static boolean canRejectDsdValidation(ProcStatusEnum procStatus) {
-        return SharedDsdSecurityUtils.canRejectDsdValidation(MetamacSrmWeb.getCurrentUser(), procStatus);
+        return true;
+        // return SharedDsdSecurityUtils.canRejectDsdValidation(MetamacSrmWeb.getCurrentUser(), procStatus);
     }
 
     public static boolean canPublishDsdInternally() {
-        return SharedDsdSecurityUtils.canPublishDsdInternally(MetamacSrmWeb.getCurrentUser());
+        return true;
+        // return SharedDsdSecurityUtils.canPublishDsdInternally(MetamacSrmWeb.getCurrentUser());
     }
 
     public static boolean canPublishDsdExternally() {
-        return SharedDsdSecurityUtils.canPublishDsdExternally(MetamacSrmWeb.getCurrentUser());
+        return true;
+        // return SharedDsdSecurityUtils.canPublishDsdExternally(MetamacSrmWeb.getCurrentUser());
     }
-
 }
