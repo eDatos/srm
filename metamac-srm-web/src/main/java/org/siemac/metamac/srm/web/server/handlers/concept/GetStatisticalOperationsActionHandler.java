783a06ebe6565479b950349f55c4c71f89915a93
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-14 13:51:32 +0000

    METAMAC-1482 METAMAC-1179 Obtener las operaciones estadísticas sobre las que un usuario tiene permiso
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43694 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/concept/GetStatisticalOperationsActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/concept/GetStatisticalOperationsActionHandler.java
@@ -9,8 +9,6 @@ import org.siemac.metamac.core.common.conf.ConfigurationService;
 import org.siemac.metamac.core.common.dto.ExternalItemDto;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.Operations;
-import org.siemac.metamac.srm.core.constants.SrmConstants;
-import org.siemac.metamac.srm.core.enume.domain.SrmRoleEnum;
 import org.siemac.metamac.srm.core.security.shared.SharedSecurityUtils;
 import org.siemac.metamac.srm.web.client.constants.SrmWebConstants;
 import org.siemac.metamac.srm.web.server.rest.RestApiConstants;
@@ -19,7 +17,6 @@ import org.siemac.metamac.srm.web.server.utils.ExternalItemUtils;
 import org.siemac.metamac.srm.web.shared.concept.GetStatisticalOperationsAction;
 import org.siemac.metamac.srm.web.shared.concept.GetStatisticalOperationsResult;
 import org.siemac.metamac.sso.client.MetamacPrincipal;
-import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
 import org.siemac.metamac.sso.utils.SecurityUtils;
 import org.siemac.metamac.web.common.server.ServiceContextHolder;
 import org.siemac.metamac.web.common.server.handlers.SecurityActionHandler;
@@ -119,58 +116,9 @@ public class GetStatisticalOperationsActionHandler extends SecurityActionHandler
     private Set<String> getUserOperations() throws MetamacWebException {
         try {
             MetamacPrincipal metamacPrincipal = SecurityUtils.getMetamacPrincipal(ServiceContextHolder.getCurrentServiceContext());
-            addAccessToMetamacPrincipal(metamacPrincipal); // TODO Remove this method!
             return SharedSecurityUtils.getOperationCodesFromMetamacPrincipalInApplication(metamacPrincipal);
         } catch (MetamacException e) {
             throw new MetamacWebException(CommonSharedConstants.EXCEPTION_UNKNOWN, "Error getting MetamacPrincipal from ServiceContext");
-        }
-    }
-
-    // TODO REMOVE THIS METHOD!
-    private void addAccessToMetamacPrincipal(MetamacPrincipal metamacPrincipal) {
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.ADMINISTRADOR.toString(), SrmConstants.SECURITY_APPLICATION_ID, "r");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
-        }
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.JEFE_NORMALIZACION.toString(), SrmConstants.SECURITY_APPLICATION_ID, "op_interna");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
-        }
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.TECNICO_APOYO_NORMALIZACION.toString(), SrmConstants.SECURITY_APPLICATION_ID, "op_ex_1");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
-        }
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.TECNICO_PRODUCCION.toString(), SrmConstants.SECURITY_APPLICATION_ID, "op_ex_2");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
-        }
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.JEFE_NORMALIZACION.toString(), SrmConstants.SECURITY_APPLICATION_ID, "op_ex_3");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
-        }
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.JEFE_NORMALIZACION.toString(), SrmConstants.SECURITY_APPLICATION_ID, "op_ex_4");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
-        }
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.JEFE_NORMALIZACION.toString(), SrmConstants.SECURITY_APPLICATION_ID, "op_ex_5");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
-        }
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.JEFE_NORMALIZACION.toString(), SrmConstants.SECURITY_APPLICATION_ID, "op_ex_6");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
-        }
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.TECNICO_APOYO_NORMALIZACION.toString(), SrmConstants.SECURITY_APPLICATION_ID, "op_ex_4");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
-        }
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.TECNICO_PRODUCCION.toString(), SrmConstants.SECURITY_APPLICATION_ID, "op_ex_5");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
-        }
-        {
-            MetamacPrincipalAccess metamacPrincipalAccess = new MetamacPrincipalAccess(SrmRoleEnum.ADMINISTRADOR.toString(), SrmConstants.SECURITY_APPLICATION_ID, "op_ex_6");
-            metamacPrincipal.getAccesses().add(metamacPrincipalAccess);
         }
     }
 }
