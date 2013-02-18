402e788973cdddf79da5043d6d171d5f1b09ee7f
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-18 14:04:34 +0000

    METAMAC-893 Refactor en WebCriteria
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43777 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/shared/criteria/ConceptSchemeWebCriteria.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/shared/criteria/ConceptSchemeWebCriteria.java
@@ -1,24 +1,11 @@
 package org.siemac.metamac.srm.web.shared.criteria;
 
-import java.util.Date;
 
-import org.siemac.metamac.srm.core.enume.domain.ProcStatusEnum;
-
-public class ConceptSchemeWebCriteria extends MetamacWebCriteria {
+public class ConceptSchemeWebCriteria extends LifeCycleWebCriteria {
 
     private static final long serialVersionUID = 1L;
 
     private String            dsdUrn;
-    private Boolean           isLastVersion;
-    private String            code;
-    private String            name;
-    private String            urn;
-    private String            description;
-    private ProcStatusEnum    procStatusEnum;
-    private Date              internalPublicationDate;
-    private String            internalPublicationUser;
-    private Date              externalPublicationDate;
-    private String            externalPublicationUser;
 
     public ConceptSchemeWebCriteria() {
     }
@@ -38,85 +25,5 @@ public class ConceptSchemeWebCriteria extends MetamacWebCriteria {
 
     public void setDsdUrn(String dsdUrn) {
         this.dsdUrn = dsdUrn;
-    }
-
-    public Boolean getIsLastVersion() {
-        return isLastVersion;
-    }
-
-    public void setIsLastVersion(Boolean isLastVersion) {
-        this.isLastVersion = isLastVersion;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUrn() {
-        return urn;
-    }
-
-    public void setUrn(String urn) {
-        this.urn = urn;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public ProcStatusEnum getProcStatusEnum() {
-        return procStatusEnum;
-    }
-
-    public void setProcStatusEnum(ProcStatusEnum procStatusEnum) {
-        this.procStatusEnum = procStatusEnum;
-    }
-
-    public Date getInternalPublicationDate() {
-        return internalPublicationDate;
-    }
-
-    public String getInternalPublicationUser() {
-        return internalPublicationUser;
-    }
-
-    public Date getExternalPublicationDate() {
-        return externalPublicationDate;
-    }
-
-    public String getExternalPublicationUser() {
-        return externalPublicationUser;
-    }
-
-    public void setInternalPublicationDate(Date internalPublicationDate) {
-        this.internalPublicationDate = internalPublicationDate;
-    }
-
-    public void setInternalPublicationUser(String internalPublicationUser) {
-        this.internalPublicationUser = internalPublicationUser;
-    }
-
-    public void setExternalPublicationDate(Date externalPublicationDate) {
-        this.externalPublicationDate = externalPublicationDate;
-    }
-
-    public void setExternalPublicationUser(String externalPublicationUser) {
-        this.externalPublicationUser = externalPublicationUser;
     }
 }
---
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/shared/criteria/LifeCycleWebCriteria.java
@@ -0,0 +1,108 @@
+package org.siemac.metamac.srm.web.shared.criteria;
+
+import java.util.Date;
+
+import org.siemac.metamac.srm.core.enume.domain.ProcStatusEnum;
+
+public class LifeCycleWebCriteria extends MetamacWebCriteria {
+
+    private static final long serialVersionUID = 1L;
+
+    private String            code;
+    private String            name;
+    private String            urn;
+    private String            description;
+    private ProcStatusEnum    procStatusEnum;
+    private Date              internalPublicationDate;
+    private String            internalPublicationUser;
+    private Date              externalPublicationDate;
+    private String            externalPublicationUser;
+    private Boolean           isLastVersion;
+
+    public LifeCycleWebCriteria() {
+    }
+
+    public LifeCycleWebCriteria(String criteria) {
+        super(criteria);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrn() {
+        return urn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ProcStatusEnum getProcStatusEnum() {
+        return procStatusEnum;
+    }
+
+    public Date getInternalPublicationDate() {
+        return internalPublicationDate;
+    }
+
+    public String getInternalPublicationUser() {
+        return internalPublicationUser;
+    }
+
+    public Date getExternalPublicationDate() {
+        return externalPublicationDate;
+    }
+
+    public String getExternalPublicationUser() {
+        return externalPublicationUser;
+    }
+
+    public Boolean getIsLastVersion() {
+        return isLastVersion;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUrn(String urn) {
+        this.urn = urn;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setProcStatusEnum(ProcStatusEnum procStatusEnum) {
+        this.procStatusEnum = procStatusEnum;
+    }
+
+    public void setInternalPublicationDate(Date internalPublicationDate) {
+        this.internalPublicationDate = internalPublicationDate;
+    }
+
+    public void setInternalPublicationUser(String internalPublicationUser) {
+        this.internalPublicationUser = internalPublicationUser;
+    }
+
+    public void setExternalPublicationDate(Date externalPublicationDate) {
+        this.externalPublicationDate = externalPublicationDate;
+    }
+
+    public void setExternalPublicationUser(String externalPublicationUser) {
+        this.externalPublicationUser = externalPublicationUser;
+    }
+
+    public void setIsLastVersion(Boolean isLastVersion) {
+        this.isLastVersion = isLastVersion;
+    }
+}
