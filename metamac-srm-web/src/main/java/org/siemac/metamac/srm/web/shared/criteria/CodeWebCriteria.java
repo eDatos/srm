26d4dfe90ac3c3207020b0333b30d0deba7f474c
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-02-20 07:52:21 +0000

    METAMAC-893 Creada clase CodeWebCriteria
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@43833 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/shared/criteria/CodeWebCriteria.java
@@ -0,0 +1,13 @@
+package org.siemac.metamac.srm.web.shared.criteria;
+
+public class CodeWebCriteria extends ItemWebCriteria {
+
+    private static final long serialVersionUID = 1L;
+
+    public CodeWebCriteria() {
+    }
+
+    public CodeWebCriteria(String criteria) {
+        super(criteria);
+    }
+}
