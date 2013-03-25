98e7aa4c4de1f1d5de276199b2f296b44945391c
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-03-25 14:53:20 +0000

    METAMAC-1458 Niveles de apertura
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@44621 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/code/model/ds/CodelistVisualisationDS.java
@@ -0,0 +1,14 @@
+package org.siemac.metamac.srm.web.code.model.ds;
+
+import com.smartgwt.client.data.DataSource;
+
+public class CodelistVisualisationDS extends DataSource {
+
+    public static final String ID        = "vis-id";
+    public static final String CODE      = "vis-code";
+    public static final String CODE_VIEW = "vis-code-view";
+    public static final String NAME      = "vis-name";
+    public static final String URN       = "vis-urn";
+
+    public static final String DTO       = "vis-dto";
+}
