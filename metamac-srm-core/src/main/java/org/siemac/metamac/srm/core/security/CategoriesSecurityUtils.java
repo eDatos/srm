9c5311eb119843ae68f718892a52dcaa227e7d6e
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-04-08 12:02:19 +0000

    METAMAC-1546 Método de seguridad específico para la creación de versiones temporales
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@44899 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/security/CategoriesSecurityUtils.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/security/CategoriesSecurityUtils.java
@@ -7,6 +7,10 @@ import org.siemac.metamac.sso.utils.SecurityUtils;
 
 public class CategoriesSecurityUtils extends SecurityUtils {
 
+    //
+    // NOTE: Only to specific actions. Security about the others actions is in ItemSecurityUtils
+    //
+
     public static void canCreateCategorySchemeTemporalVersion(ServiceContext ctx) throws MetamacException {
         if (!SharedCategoriesSecurityUtils.canCreateCategorySchemeTemporalVersion(getMetamacPrincipal(ctx))) {
             throwExceptionIfOperationNotAllowed(ctx);
