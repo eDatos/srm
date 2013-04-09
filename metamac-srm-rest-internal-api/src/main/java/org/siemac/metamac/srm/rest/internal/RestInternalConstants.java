86a6b719325c599327a18a31dd7689236d0c092a
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-04-09 09:47:27 +0000

    METAMAC-1576: Quitar dependencias a rest-common de los proyectos rest-api y web-common
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@44944 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-rest-internal-api/src/main/java/org/siemac/metamac/srm/rest/internal/RestInternalConstants.java
+++ metamac-srm-rest-internal-api/src/main/java/org/siemac/metamac/srm/rest/internal/RestInternalConstants.java
@@ -1,12 +1,11 @@
 package org.siemac.metamac.srm.rest.internal;
 
-import org.siemac.metamac.rest.constants.RestConstants;
-
 public class RestInternalConstants {
 
     public static String API_NAME                               = "structuralResources";
     public static String API_VERSION_1_0                        = "v1.0";
 
+    public static String KIND_SEPARATOR                         = "#";
     public static String WILDCARD                               = "~all";
     public static String LATEST                                 = "~latest";
 
@@ -42,46 +41,46 @@ public class RestInternalConstants {
 
     public static String LINK_SUBPATH_DATA_STRUCTURES           = "datastructures";
 
-    public static String KIND_CONCEPT_SCHEMES                   = API_NAME + RestConstants.KIND_SEPARATOR + "conceptSchemes";
-    public static String KIND_CONCEPT_SCHEME                    = API_NAME + RestConstants.KIND_SEPARATOR + "conceptScheme";
-    public static String KIND_CONCEPTS                          = API_NAME + RestConstants.KIND_SEPARATOR + "concepts";
-    public static String KIND_CONCEPT                           = API_NAME + RestConstants.KIND_SEPARATOR + "concept";
-    public static String KIND_CONCEPT_TYPES                     = API_NAME + RestConstants.KIND_SEPARATOR + "conceptTypes";
+    public static String KIND_CONCEPT_SCHEMES                   = API_NAME + KIND_SEPARATOR + "conceptSchemes";
+    public static String KIND_CONCEPT_SCHEME                    = API_NAME + KIND_SEPARATOR + "conceptScheme";
+    public static String KIND_CONCEPTS                          = API_NAME + KIND_SEPARATOR + "concepts";
+    public static String KIND_CONCEPT                           = API_NAME + KIND_SEPARATOR + "concept";
+    public static String KIND_CONCEPT_TYPES                     = API_NAME + KIND_SEPARATOR + "conceptTypes";
 
-    public static String KIND_CATEGORY_SCHEMES                  = API_NAME + RestConstants.KIND_SEPARATOR + "categorySchemes";
-    public static String KIND_CATEGORY_SCHEME                   = API_NAME + RestConstants.KIND_SEPARATOR + "categoryScheme";
-    public static String KIND_CATEGORIES                        = API_NAME + RestConstants.KIND_SEPARATOR + "categories";
-    public static String KIND_CATEGORY                          = API_NAME + RestConstants.KIND_SEPARATOR + "category";
-    public static String KIND_CATEGORISATIONS                   = API_NAME + RestConstants.KIND_SEPARATOR + "categorisations";
-    public static String KIND_CATEGORISATION                    = API_NAME + RestConstants.KIND_SEPARATOR + "categorisation";
+    public static String KIND_CATEGORY_SCHEMES                  = API_NAME + KIND_SEPARATOR + "categorySchemes";
+    public static String KIND_CATEGORY_SCHEME                   = API_NAME + KIND_SEPARATOR + "categoryScheme";
+    public static String KIND_CATEGORIES                        = API_NAME + KIND_SEPARATOR + "categories";
+    public static String KIND_CATEGORY                          = API_NAME + KIND_SEPARATOR + "category";
+    public static String KIND_CATEGORISATIONS                   = API_NAME + KIND_SEPARATOR + "categorisations";
+    public static String KIND_CATEGORISATION                    = API_NAME + KIND_SEPARATOR + "categorisation";
 
-    public static String KIND_ORGANISATION_SCHEMES              = API_NAME + RestConstants.KIND_SEPARATOR + "organisationSchemes";
-    public static String KIND_AGENCY_SCHEMES                    = API_NAME + RestConstants.KIND_SEPARATOR + "agencySchemes";
-    public static String KIND_ORGANISATION_UNIT_SCHEMES         = API_NAME + RestConstants.KIND_SEPARATOR + "organisationUnitSchemes";
-    public static String KIND_DATA_PROVIDER_SCHEMES             = API_NAME + RestConstants.KIND_SEPARATOR + "dataProviderSchemes";
-    public static String KIND_DATA_CONSUMER_SCHEMES             = API_NAME + RestConstants.KIND_SEPARATOR + "dataConsumerSchemes";
+    public static String KIND_ORGANISATION_SCHEMES              = API_NAME + KIND_SEPARATOR + "organisationSchemes";
+    public static String KIND_AGENCY_SCHEMES                    = API_NAME + KIND_SEPARATOR + "agencySchemes";
+    public static String KIND_ORGANISATION_UNIT_SCHEMES         = API_NAME + KIND_SEPARATOR + "organisationUnitSchemes";
+    public static String KIND_DATA_PROVIDER_SCHEMES             = API_NAME + KIND_SEPARATOR + "dataProviderSchemes";
+    public static String KIND_DATA_CONSUMER_SCHEMES             = API_NAME + KIND_SEPARATOR + "dataConsumerSchemes";
 
-    public static String KIND_AGENCY_SCHEME                     = API_NAME + RestConstants.KIND_SEPARATOR + "agencyScheme";
-    public static String KIND_ORGANISATION_UNIT_SCHEME          = API_NAME + RestConstants.KIND_SEPARATOR + "organisationUnitScheme";
-    public static String KIND_DATA_PROVIDER_SCHEME              = API_NAME + RestConstants.KIND_SEPARATOR + "dataProviderScheme";
-    public static String KIND_DATA_CONSUMER_SCHEME              = API_NAME + RestConstants.KIND_SEPARATOR + "dataConsumerScheme";
+    public static String KIND_AGENCY_SCHEME                     = API_NAME + KIND_SEPARATOR + "agencyScheme";
+    public static String KIND_ORGANISATION_UNIT_SCHEME          = API_NAME + KIND_SEPARATOR + "organisationUnitScheme";
+    public static String KIND_DATA_PROVIDER_SCHEME              = API_NAME + KIND_SEPARATOR + "dataProviderScheme";
+    public static String KIND_DATA_CONSUMER_SCHEME              = API_NAME + KIND_SEPARATOR + "dataConsumerScheme";
 
-    public static String KIND_ORGANISATIONS                     = API_NAME + RestConstants.KIND_SEPARATOR + "organisations";
-    public static String KIND_AGENCIES                          = API_NAME + RestConstants.KIND_SEPARATOR + "agencies";
-    public static String KIND_ORGANISATION_UNITS                = API_NAME + RestConstants.KIND_SEPARATOR + "organisationUnits";
-    public static String KIND_DATA_PROVIDERS                    = API_NAME + RestConstants.KIND_SEPARATOR + "dataProviders";
-    public static String KIND_DATA_CONSUMERS                    = API_NAME + RestConstants.KIND_SEPARATOR + "dataConsumers";
+    public static String KIND_ORGANISATIONS                     = API_NAME + KIND_SEPARATOR + "organisations";
+    public static String KIND_AGENCIES                          = API_NAME + KIND_SEPARATOR + "agencies";
+    public static String KIND_ORGANISATION_UNITS                = API_NAME + KIND_SEPARATOR + "organisationUnits";
+    public static String KIND_DATA_PROVIDERS                    = API_NAME + KIND_SEPARATOR + "dataProviders";
+    public static String KIND_DATA_CONSUMERS                    = API_NAME + KIND_SEPARATOR + "dataConsumers";
 
-    public static String KIND_AGENCY                            = API_NAME + RestConstants.KIND_SEPARATOR + "agency";
-    public static String KIND_ORGANISATION_UNIT                 = API_NAME + RestConstants.KIND_SEPARATOR + "organisationUnit";
-    public static String KIND_DATA_PROVIDER                     = API_NAME + RestConstants.KIND_SEPARATOR + "dataProvider";
-    public static String KIND_DATA_CONSUMER                     = API_NAME + RestConstants.KIND_SEPARATOR + "dataConsumer";
+    public static String KIND_AGENCY                            = API_NAME + KIND_SEPARATOR + "agency";
+    public static String KIND_ORGANISATION_UNIT                 = API_NAME + KIND_SEPARATOR + "organisationUnit";
+    public static String KIND_DATA_PROVIDER                     = API_NAME + KIND_SEPARATOR + "dataProvider";
+    public static String KIND_DATA_CONSUMER                     = API_NAME + KIND_SEPARATOR + "dataConsumer";
 
-    public static String KIND_CODELISTS                         = API_NAME + RestConstants.KIND_SEPARATOR + "codelists";
-    public static String KIND_CODELIST                          = API_NAME + RestConstants.KIND_SEPARATOR + "codelist";
-    public static String KIND_CODES                             = API_NAME + RestConstants.KIND_SEPARATOR + "codes";
-    public static String KIND_CODE                              = API_NAME + RestConstants.KIND_SEPARATOR + "code";
+    public static String KIND_CODELISTS                         = API_NAME + KIND_SEPARATOR + "codelists";
+    public static String KIND_CODELIST                          = API_NAME + KIND_SEPARATOR + "codelist";
+    public static String KIND_CODES                             = API_NAME + KIND_SEPARATOR + "codes";
+    public static String KIND_CODE                              = API_NAME + KIND_SEPARATOR + "code";
 
-    public static String KIND_DATA_STRUCTURES                   = API_NAME + RestConstants.KIND_SEPARATOR + "dataStructures";
-    public static String KIND_DATA_STRUCTURE                    = API_NAME + RestConstants.KIND_SEPARATOR + "dataStructure";
+    public static String KIND_DATA_STRUCTURES                   = API_NAME + KIND_SEPARATOR + "dataStructures";
+    public static String KIND_DATA_STRUCTURE                    = API_NAME + KIND_SEPARATOR + "dataStructure";
 }
