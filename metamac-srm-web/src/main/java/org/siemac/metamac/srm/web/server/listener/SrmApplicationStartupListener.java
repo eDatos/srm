97900fea913bf4fb9cf5dcd18b10ace1fcfcd39c
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-04-15 06:59:05 +0000

    METAMAC-1564 Listener para comprobar propiedades de configuración del data
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@45103 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/listener/SrmApplicationStartupListener.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/listener/SrmApplicationStartupListener.java
@@ -1,10 +1,42 @@
 package org.siemac.metamac.srm.web.server.listener;
 
+import javax.servlet.ServletContextEvent;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
 import org.siemac.metamac.core.common.constants.shared.ConfigurationConstants;
+import org.siemac.metamac.core.common.criteria.MetamacCriteria;
+import org.siemac.metamac.core.common.criteria.MetamacCriteriaPaginator;
+import org.siemac.metamac.core.common.criteria.MetamacCriteriaPropertyRestriction;
+import org.siemac.metamac.core.common.criteria.MetamacCriteriaPropertyRestriction.OperationType;
+import org.siemac.metamac.core.common.criteria.MetamacCriteriaResult;
+import org.siemac.metamac.core.common.exception.MetamacException;
+import org.siemac.metamac.core.common.util.ApplicationContextProvider;
 import org.siemac.metamac.srm.core.constants.SrmConfigurationConstants;
+import org.siemac.metamac.srm.core.constants.SrmConstants;
+import org.siemac.metamac.srm.core.criteria.OrganisationMetamacCriteriaPropertyEnum;
+import org.siemac.metamac.srm.core.enume.domain.SrmRoleEnum;
+import org.siemac.metamac.srm.core.facade.serviceapi.SrmCoreServiceFacade;
+import org.siemac.metamac.srm.core.organisation.dto.OrganisationMetamacDto;
+import org.siemac.metamac.srm.web.client.constants.SrmWebConstants;
+import org.siemac.metamac.sso.client.MetamacPrincipal;
+import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
+import org.siemac.metamac.sso.client.SsoClientConstants;
 import org.siemac.metamac.web.common.server.listener.ApplicationStartupListener;
 
 public class SrmApplicationStartupListener extends ApplicationStartupListener {
+
+    private static final Log     LOG = LogFactory.getLog(SrmApplicationStartupListener.class);
+
+    private SrmCoreServiceFacade srmCoreServiceFacade;
+    private ServiceContext       serviceContext;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        srmCoreServiceFacade = ApplicationContextProvider.getApplicationContext().getBean(SrmCoreServiceFacade.class);
+        super.contextInitialized(sce);
+    }
 
     @Override
     public void checkConfiguration() {
@@ -31,15 +63,59 @@ public class SrmApplicationStartupListener extends ApplicationStartupListener {
 
         // OTHER CONFIGURATION PROPERTIES
 
+        // Common properties
+
         checkRequiredProperty(ConfigurationConstants.METAMAC_EDITION_LANGUAGES);
         checkRequiredProperty(ConfigurationConstants.METAMAC_NAVBAR_URL);
         checkRequiredProperty(ConfigurationConstants.METAMAC_ORGANISATION);
-        checkRequiredProperty(ConfigurationConstants.METAMAC_ORGANISATION_URN);
+        checkOrganisationUrnProperty();
+
+        // SRM properties
 
         checkRequiredProperty(SrmConfigurationConstants.USER_GUIDE_FILE_NAME);
 
         checkOptionalProperty(SrmConfigurationConstants.DSD_PRIMARY_MEASURE_DEFAULT_CONCEPT_ID_URN);
         checkOptionalProperty(SrmConfigurationConstants.DSD_TIME_DIMENSION_DEFAULT_CONCEPT_ID_URN);
         checkOptionalProperty(SrmConfigurationConstants.DSD_MEASURE_DIMENSION_DEFAULT_CONCEPT_ID_URN);
+    }
+
+    private void checkOrganisationUrnProperty() {
+        String propertyKey = ConfigurationConstants.METAMAC_ORGANISATION_URN;
+        checkRequiredProperty(propertyKey);
+
+        String organisationUrn = configurationService.getProperty(propertyKey);
+        try {
+            MetamacCriteria criteria = new MetamacCriteria();
+            criteria.setPaginator(new MetamacCriteriaPaginator());
+            criteria.getPaginator().setFirstResult(0);
+            criteria.getPaginator().setMaximumResultSize(SrmWebConstants.NO_LIMIT_IN_PAGINATION);
+            criteria.getPaginator().setCountTotalResults(true);
+            criteria.setRestriction(new MetamacCriteriaPropertyRestriction(OrganisationMetamacCriteriaPropertyEnum.URN.name(), organisationUrn, OperationType.EQ));
+
+            MetamacCriteriaResult<OrganisationMetamacDto> result = srmCoreServiceFacade.findOrganisationsByCondition(getStartupServiceContext(), criteria);
+            if (result.getResults().isEmpty()) {
+                String errorMessage = "Property [" + propertyKey + "] is not properly filled. The organisation URN specified is not correct. Aborting application startup...";
+                abortApplicationStartup(errorMessage);
+            } else {
+                LOG.info("Property [" + propertyKey + "] filled");
+            }
+        } catch (MetamacException e) {
+            abortApplicationStartup("Error checking the property [" + propertyKey + "]");
+        }
+    }
+
+    private ServiceContext getStartupServiceContext() {
+        if (serviceContext == null) {
+            String userId = "SRM_STARTUP_USER";
+            String sessionId = "SRM_STARTUP_SESSION_ID";
+
+            MetamacPrincipal metamacPrincipal = new MetamacPrincipal();
+            metamacPrincipal.setUserId(userId);
+            metamacPrincipal.getAccesses().add(new MetamacPrincipalAccess(SrmRoleEnum.ADMINISTRADOR.getName(), SrmConstants.SECURITY_APPLICATION_ID, null));
+
+            serviceContext = new ServiceContext(metamacPrincipal.getUserId(), sessionId, SrmConstants.SECURITY_APPLICATION_ID);
+            serviceContext.setProperty(SsoClientConstants.PRINCIPAL_ATTRIBUTE, metamacPrincipal);
+        }
+        return serviceContext;
     }
 }
