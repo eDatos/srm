b8260e71f07f4dbb0fb42dba7e4fabd94c85abdc
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-04-25 13:50:55 +0000

    METAMAC-1636 Construir los árboles de Items linkando los nodos por el padre, no especificando los hijos
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@45438 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/widgets/ItemsTreeGrid.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/widgets/ItemsTreeGrid.java
@@ -3,10 +3,8 @@ package org.siemac.metamac.srm.web.client.widgets;
 import java.util.List;
 
 import org.siemac.metamac.srm.web.client.model.ds.ItemDS;
-import org.siemac.metamac.web.common.client.utils.InternationalStringUtils;
 
-import com.arte.statistic.sdmx.v2_1.domain.dto.srm.ItemDto;
-import com.arte.statistic.sdmx.v2_1.domain.dto.srm.ItemHierarchyDto;
+import com.arte.statistic.sdmx.srm.core.common.domain.shared.ItemVisualisationResult;
 import com.arte.statistic.sdmx.v2_1.domain.dto.srm.ItemSchemeDto;
 import com.smartgwt.client.types.TreeModelType;
 import com.smartgwt.client.widgets.tree.Tree;
@@ -26,56 +24,51 @@ public abstract class ItemsTreeGrid extends BaseItemsTreeGrid {
 
             @Override
             public void onFolderContextClick(final FolderContextClickEvent event) {
-                onNodeContextClick(event.getFolder().getName(), (ItemDto) event.getFolder().getAttributeAsObject(ItemDS.DTO));
+                onNodeContextClick(event.getFolder().getName(), (ItemVisualisationResult) event.getFolder().getAttributeAsObject(ItemDS.DTO));
             }
         });
         leafContextHandlerRegistration = addLeafContextClickHandler(new LeafContextClickHandler() {
 
             @Override
             public void onLeafContextClick(LeafContextClickEvent event) {
-                onNodeContextClick(event.getLeaf().getName(), (ItemDto) event.getLeaf().getAttributeAsObject(ItemDS.DTO));
+                onNodeContextClick(event.getLeaf().getName(), (ItemVisualisationResult) event.getLeaf().getAttributeAsObject(ItemDS.DTO));
             }
         });
     }
 
-    public void setItems(ItemSchemeDto itemSchemeDto, List<ItemHierarchyDto> itemHierarchyDtos) {
+    public void setItems(ItemSchemeDto itemSchemeDto, List<ItemVisualisationResult> itemVisualisationResults) {
         // Clear filter editor
         setFilterEditorCriteria(null);
 
         this.itemSchemeDto = itemSchemeDto;
 
-        TreeNode[] treeNodes = new TreeNode[itemHierarchyDtos.size()];
-        for (int i = 0; i < itemHierarchyDtos.size(); i++) {
-            treeNodes[i] = createItemTreeNode(itemHierarchyDtos.get(i), null);
+        TreeNode[] treeNodes = new TreeNode[itemVisualisationResults.size() + 1];
+        treeNodes[0] = createItemSchemeTreeNode(itemSchemeDto);
+
+        for (int i = 0; i < itemVisualisationResults.size(); i++) {
+            treeNodes[i + 1] = createItemTreeNode(itemVisualisationResults.get(i));
         }
 
-        TreeNode organisationSchemeTreeNode = createItemSchemeTreeNode(itemSchemeDto);
-        organisationSchemeTreeNode.setChildren(treeNodes);
-
         tree = new Tree();
-        tree.setModelType(TreeModelType.CHILDREN);
-        tree.setData(new TreeNode[]{organisationSchemeTreeNode});
+        tree.setModelType(TreeModelType.PARENT);
+        tree.linkNodes(treeNodes);
         setData(tree);
         getData().openAll();
     }
 
-    protected TreeNode createItemTreeNode(ItemHierarchyDto itemHierarchyDto, String itemParentUrn) {
-        TreeNode node = new TreeNode(itemHierarchyDto.getItem().getId().toString());
-        node.setAttribute(ItemDS.CODE, itemHierarchyDto.getItem().getCode());
-        node.setAttribute(ItemDS.NAME, InternationalStringUtils.getLocalisedString(itemHierarchyDto.getItem().getName()));
-        node.setAttribute(ItemDS.URN, itemHierarchyDto.getItem().getUrn());
-        node.setAttribute(ItemDS.ITEM_PARENT_URN, itemParentUrn);
-        node.setAttribute(ItemDS.DTO, itemHierarchyDto.getItem());
+    protected TreeNode createItemTreeNode(ItemVisualisationResult itemVisualisationResult) {
+        String parentUrn = itemVisualisationResult.getParent() != null ? itemVisualisationResult.getParent().getUrn() : SCHEME_NODE_NAME;
 
-        // Node children
-        TreeNode[] children = new TreeNode[itemHierarchyDto.getChildren().size()];
-        for (int i = 0; i < itemHierarchyDto.getChildren().size(); i++) {
-            children[i] = createItemTreeNode(itemHierarchyDto.getChildren().get(i), itemHierarchyDto.getItem().getUrn());
-        }
-        node.setChildren(children);
-
+        TreeNode node = new TreeNode(itemVisualisationResult.getItemIdDatabase().toString());
+        node.setID(itemVisualisationResult.getUrn());
+        node.setParentID(parentUrn);
+        node.setAttribute(ItemDS.CODE, itemVisualisationResult.getCode());
+        node.setAttribute(ItemDS.NAME, itemVisualisationResult.getName());
+        node.setAttribute(ItemDS.URN, itemVisualisationResult.getUrn());
+        node.setAttribute(ItemDS.ITEM_PARENT_URN, parentUrn);
+        node.setAttribute(ItemDS.DTO, itemVisualisationResult);
         return node;
     }
 
-    protected abstract void onNodeContextClick(String nodeName, ItemDto itemDto);
+    protected abstract void onNodeContextClick(String nodeName, ItemVisualisationResult itemVisualisationResult);
 }
