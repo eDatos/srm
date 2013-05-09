8679acb2857c1a62481a454edcecd53d269b5b35
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-05-09 08:09:19 +0000

    METAMAC-1624 Utilidades para parsear URNs
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@45702 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/service/utils/shared/SrmUrnParserUtils.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/service/utils/shared/SrmUrnParserUtils.java
@@ -1,5 +1,6 @@
 package org.siemac.metamac.srm.core.common.service.utils.shared;
 
+import static org.siemac.metamac.core.common.constants.shared.UrnConstants.EQUAL;
 import static org.siemac.metamac.core.common.constants.shared.UrnConstants.URN_SIEMAC_CLASS_CODELIST_FAMILY_PREFIX;
 import static org.siemac.metamac.core.common.constants.shared.UrnConstants.URN_SIEMAC_CLASS_CODELIST_VARIABLE_ELEMENT_PREFIX;
 import static org.siemac.metamac.core.common.constants.shared.UrnConstants.URN_SIEMAC_CLASS_CODELIST_VARIABLE_FAMILY_PREFIX;
@@ -27,5 +28,10 @@ public class SrmUrnParserUtils extends SdmxSrmUrnParserUtils {
 
     public static boolean isVariableElementUrn(String urn) {
         return matches(URN_SIEMAC_CLASS_CODELIST_VARIABLE_ELEMENT_PREFIX, UrnUtils.extractPrefix(urn));
+    }
+
+    public static String getVariableUrnFromVariableElementUrn(String variableElementUrn) {
+        String variableIdentifier = UrnUtils.splitUrnByDots(UrnUtils.removePrefix(variableElementUrn))[0];
+        return URN_SIEMAC_CLASS_CODELIST_VARIABLE_PREFIX + EQUAL + variableIdentifier;
     }
 }
