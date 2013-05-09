89e5c05590150ce919cbaa2e3bfc7e64920115b0
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-05-09 07:21:05 +0000

    METAMAC-1624 Utilidades para parsear URNs
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@45696 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/service/utils/shared/SrmUrnParserUtils.java
+++ metamac-srm-core/src/main/java/org/siemac/metamac/srm/core/common/service/utils/shared/SrmUrnParserUtils.java
@@ -5,7 +5,6 @@ import static org.siemac.metamac.core.common.constants.shared.UrnConstants.URN_S
 import static org.siemac.metamac.core.common.constants.shared.UrnConstants.URN_SIEMAC_CLASS_CODELIST_VARIABLE_FAMILY_PREFIX;
 import static org.siemac.metamac.core.common.constants.shared.UrnConstants.URN_SIEMAC_CLASS_CODELIST_VARIABLE_PREFIX;
 
-import org.siemac.metamac.core.common.util.shared.StringUtils;
 import org.siemac.metamac.core.common.util.shared.UrnUtils;
 
 import com.arte.statistic.sdmx.srm.core.common.service.utils.shared.SdmxSrmUrnParserUtils;
@@ -15,30 +14,18 @@ public class SrmUrnParserUtils extends SdmxSrmUrnParserUtils {
     // CODES
 
     public static boolean isCodelistFamilyUrn(String urn) {
-        if (StringUtils.isBlank(urn)) {
-            throw new IllegalArgumentException("URN can not be null");
-        }
-        return StringUtils.equals(URN_SIEMAC_CLASS_CODELIST_FAMILY_PREFIX, UrnUtils.extractPrefix(urn));
+        return matches(URN_SIEMAC_CLASS_CODELIST_FAMILY_PREFIX, UrnUtils.extractPrefix(urn));
     }
 
     public static boolean isVariableFamilyUrn(String urn) {
-        if (StringUtils.isBlank(urn)) {
-            throw new IllegalArgumentException("URN can not be null");
-        }
-        return StringUtils.equals(URN_SIEMAC_CLASS_CODELIST_VARIABLE_FAMILY_PREFIX, UrnUtils.extractPrefix(urn));
+        return matches(URN_SIEMAC_CLASS_CODELIST_VARIABLE_FAMILY_PREFIX, UrnUtils.extractPrefix(urn));
     }
 
     public static boolean isVariableUrn(String urn) {
-        if (StringUtils.isBlank(urn)) {
-            throw new IllegalArgumentException("URN can not be null");
-        }
-        return StringUtils.equals(URN_SIEMAC_CLASS_CODELIST_VARIABLE_PREFIX, UrnUtils.extractPrefix(urn));
+        return matches(URN_SIEMAC_CLASS_CODELIST_VARIABLE_PREFIX, UrnUtils.extractPrefix(urn));
     }
 
     public static boolean isVariableElementUrn(String urn) {
-        if (StringUtils.isBlank(urn)) {
-            throw new IllegalArgumentException("URN can not be null");
-        }
-        return StringUtils.equals(URN_SIEMAC_CLASS_CODELIST_VARIABLE_ELEMENT_PREFIX, UrnUtils.extractPrefix(urn));
+        return matches(URN_SIEMAC_CLASS_CODELIST_VARIABLE_ELEMENT_PREFIX, UrnUtils.extractPrefix(urn));
     }
 }
