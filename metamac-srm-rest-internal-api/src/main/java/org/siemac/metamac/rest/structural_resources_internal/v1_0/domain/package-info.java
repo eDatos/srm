d9001dfded9b4abf0308af9854e72658f20b5e73
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-05-13 13:36:18 +0000

    Separación de proyecto api por dependencia cíclica
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@45847 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-rest-internal-api/src/main/java/org/siemac/metamac/rest/structural_resources_internal/v1_0/domain/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.siemac.org/metamac/rest/structural-resources-internal/v1.0/domain", xmlns = {@javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.siemac.org/metamac/rest/structural-resources-internal/v1.0/domain", prefix = "sr")})
-package org.siemac.metamac.rest.structural_resources_internal.v1_0.domain;
-
