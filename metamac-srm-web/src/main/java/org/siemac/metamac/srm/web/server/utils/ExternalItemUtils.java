28ca6978ec463e2d0c902db2502ead62b5f47cdc
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-05-15 12:41:36 +0000

    METAMAC-1618 External Items linkables
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@45920 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/utils/ExternalItemUtils.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/utils/ExternalItemUtils.java
@@ -3,31 +3,27 @@ package org.siemac.metamac.srm.web.server.utils;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
 import org.siemac.metamac.core.common.dto.ExternalItemDto;
 import org.siemac.metamac.core.common.enume.domain.TypeExternalArtefactsEnum;
-import org.siemac.metamac.rest.common.v1_0.domain.Resource;
 import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.ResourceInternal;
 import org.siemac.metamac.web.common.server.utils.DtoUtils;
 
 public class ExternalItemUtils {
 
-    public static ExternalItemDto getOperationAsExternalItemDto(Resource resource, String operationsApiEndpoint) {
-        // Do not store REST API end point
-        String uri = StringUtils.removeStart(resource.getSelfLink().getHref(), operationsApiEndpoint);
-
+    public static ExternalItemDto getOperationAsExternalItemDto(ResourceInternal resource, String operationsApiEndpoint) {
         ExternalItemDto externalItemDto = new ExternalItemDto();
         externalItemDto.setCode(resource.getId());
-        externalItemDto.setUri(uri);
+        externalItemDto.setUri(resource.getSelfLink().getHref());
         externalItemDto.setUrn(resource.getUrn());
         externalItemDto.setType(TypeExternalArtefactsEnum.STATISTICAL_OPERATION);
         externalItemDto.setTitle(DtoUtils.getInternationalStringDtoFromInternationalString(resource.getTitle()));
+        externalItemDto.setManagementAppUrl(resource.getManagementAppLink());
         return externalItemDto;
     }
 
     public static List<ExternalItemDto> getOperationsAsExternalItemDtos(List<ResourceInternal> resources, String operationsApiEndpoint) {
         List<ExternalItemDto> externalItemDtos = new ArrayList<ExternalItemDto>(resources.size());
-        for (Resource resource : resources) {
+        for (ResourceInternal resource : resources) {
             externalItemDtos.add(getOperationAsExternalItemDto(resource, operationsApiEndpoint));
         }
         return externalItemDtos;
