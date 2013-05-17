3ffff430e92a3f694fceae715c61f8a6d7897f48
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-05-17 12:12:52 +0000

    METAMAC-1624 Related resource linkables en árboles
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@46005 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/widgets/NavigableTreeGrid.java
@@ -0,0 +1,16 @@
+package org.siemac.metamac.srm.web.client.widgets;
+
+import java.util.List;
+
+import org.siemac.metamac.srm.web.client.utils.PlaceRequestUtils;
+
+import com.arte.statistic.sdmx.v2_1.domain.dto.common.RelatedResourceDto;
+import com.gwtplatform.mvp.client.proxy.PlaceRequest;
+
+public class NavigableTreeGrid extends BaseNavigableTreeGrid<RelatedResourceDto> {
+
+    @Override
+    protected List<PlaceRequest> buildLocation(RelatedResourceDto relatedResourceDto) {
+        return PlaceRequestUtils.buildAbsoluteResourcePlaceRequest(relatedResourceDto);
+    }
+}
