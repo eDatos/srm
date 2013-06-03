d339d88102d797807b7901bb48ae29c2e412ee06
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-03 07:44:19 +0000

    METAMAC-1208 Permitir terminar la validez de las categorizaciones
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@46289 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/category/CancelCategorisationValidityActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/category/CancelCategorisationValidityActionHandler.java
@@ -1,11 +1,15 @@
 package org.siemac.metamac.srm.web.server.handlers.category;
 
+import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.srm.core.facade.serviceapi.SrmCoreServiceFacade;
 import org.siemac.metamac.srm.web.shared.category.CancelCategorisationValidityAction;
 import org.siemac.metamac.srm.web.shared.category.CancelCategorisationValidityResult;
+import org.siemac.metamac.web.common.server.ServiceContextHolder;
 import org.siemac.metamac.web.common.server.handlers.SecurityActionHandler;
+import org.siemac.metamac.web.common.server.utils.WebExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.arte.statistic.sdmx.v2_1.domain.dto.category.CategorisationDto;
 import com.gwtplatform.dispatch.shared.ActionException;
 
 public class CancelCategorisationValidityActionHandler extends SecurityActionHandler<CancelCategorisationValidityAction, CancelCategorisationValidityResult> {
@@ -19,11 +23,11 @@ public class CancelCategorisationValidityActionHandler extends SecurityActionHan
 
     @Override
     public CancelCategorisationValidityResult executeSecurityAction(CancelCategorisationValidityAction action) throws ActionException {
-        // try {
-        // TODO
-        // } catch (MetamacException e) {
-        // throw WebExceptionUtils.createMetamacWebException(e);
-        // }
-        return null;
+        try {
+            CategorisationDto categorisationDto = srmCoreServiceFacade.endCategorisationValidity(ServiceContextHolder.getCurrentServiceContext(), action.getUrn(), action.getValidTo());
+            return new CancelCategorisationValidityResult(categorisationDto);
+        } catch (MetamacException e) {
+            throw WebExceptionUtils.createMetamacWebException(e);
+        }
     }
 }
