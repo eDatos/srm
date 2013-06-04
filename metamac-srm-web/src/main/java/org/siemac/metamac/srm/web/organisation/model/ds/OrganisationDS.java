c784cfdae49cfab76769cf1a769c65e1f6e8b896
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-04 11:22:07 +0000

    METAMAC-1684 [ORGANIZACIONES] Gestión de organizaciones (que no son OrganisationUnit)
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@46360 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/organisation/model/ds/OrganisationDS.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/organisation/model/ds/OrganisationDS.java
@@ -5,6 +5,8 @@ import org.siemac.metamac.srm.web.client.model.ds.ItemDS;
 public class OrganisationDS extends ItemDS {
 
     // CONTENT DESCRIPTORS
-    public static final String TYPE      = "org-type";
-    public static final String TYPE_NAME = "org-type-name";
+    public static final String TYPE                                    = "org-type";
+    public static final String TYPE_NAME                               = "org-type-name";
+
+    public static final String SPECIAL_ORGANISATION_HAS_BEEN_PUBLISHED = "org-has-been-pub";
 }
--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/organisation/model/record/OrganisationRecord.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/organisation/model/record/OrganisationRecord.java
@@ -1,6 +1,5 @@
 package org.siemac.metamac.srm.web.organisation.model.record;
 
-import org.siemac.metamac.srm.core.organisation.dto.OrganisationMetamacBasicDto;
 import org.siemac.metamac.srm.web.client.model.record.ItemRecord;
 import org.siemac.metamac.srm.web.organisation.model.ds.OrganisationDS;
 
@@ -23,11 +22,11 @@ public class OrganisationRecord extends ItemRecord {
         return (OrganisationTypeEnum) getAttributeAsObject(OrganisationDS.TYPE);
     }
 
-    public void setOrganisationBasicDto(OrganisationMetamacBasicDto organisationDto) {
-        setAttribute(OrganisationDS.DTO, organisationDto);
+    public void setOrganisationSpecialHasBeenPublished(Boolean value) {
+        setAttribute(OrganisationDS.SPECIAL_ORGANISATION_HAS_BEEN_PUBLISHED, value);
     }
 
-    public OrganisationMetamacBasicDto getOrganisationBasicDto() {
-        return (OrganisationMetamacBasicDto) getAttributeAsObject(OrganisationDS.DTO);
+    public Boolean getSpecialOrganisationHasBeenPublished() {
+        return getAttributeAsBoolean(OrganisationDS.SPECIAL_ORGANISATION_HAS_BEEN_PUBLISHED);
     }
 }
