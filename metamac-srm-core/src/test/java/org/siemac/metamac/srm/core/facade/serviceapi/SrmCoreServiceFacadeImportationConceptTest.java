c2944c3bc0d682aa3bb53c1d9d1c9615ce8580cd
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-06 08:52:23 +0000

    CAMBIO IMPORTANTE: METAMAC-1693: Mejorar rendimiento: Eliminar relaciones a clases abstractas Item e ItemSchemeVersion
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@46531 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/facade/serviceapi/SrmCoreServiceFacadeImportationConceptTest.java
+++ metamac-srm-core/src/test/java/org/siemac/metamac/srm/core/facade/serviceapi/SrmCoreServiceFacadeImportationConceptTest.java
@@ -166,9 +166,9 @@ public class SrmCoreServiceFacadeImportationConceptTest extends SrmBaseTest {
 
         ConceptSchemeVersionMetamac conceptSchemeVersion = conceptsMetamacService.retrieveConceptSchemeByUrn(getServiceContextAdministrador(), conceptSchemeUrnExpected);
         assertEquals(3, conceptSchemeVersion.getItems().size());
-        assertListContainsItemMetamacBasicDto(conceptSchemeVersion.getItems(), urnExpectedConcept1);
-        assertListContainsItemMetamacBasicDto(conceptSchemeVersion.getItems(), urnExpectedConcept2);
-        assertListContainsItemMetamacBasicDto(conceptSchemeVersion.getItems(), urnExpectedConcept3);
+        assertListConceptsContainsConcept(conceptSchemeVersion.getItems(), urnExpectedConcept1);
+        assertListConceptsContainsConcept(conceptSchemeVersion.getItems(), urnExpectedConcept2);
+        assertListConceptsContainsConcept(conceptSchemeVersion.getItems(), urnExpectedConcept3);
 
         // Validate quantity
         {
