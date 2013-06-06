fe5790ddea06f4eeee6ca53860e00da8b7a7ab86
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-06 17:40:33 +0000

    METAMAC-1695 External Item con URN opcional
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@46556 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/widgets/RelatedResourceListItem.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/widgets/RelatedResourceListItem.java
@@ -1,13 +1,16 @@
 package org.siemac.metamac.srm.web.client.widgets;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.siemac.metamac.srm.web.client.utils.PlaceRequestUtils;
+import org.siemac.metamac.web.common.client.model.record.RelatedResourceBaseRecord;
 import org.siemac.metamac.web.common.client.widgets.form.fields.RelatedResourceBaseListItem;
 import org.siemac.metamac.web.common.client.widgets.handlers.ListRecordNavigationClickHandler;
 
 import com.arte.statistic.sdmx.v2_1.domain.dto.common.RelatedResourceDto;
 import com.gwtplatform.mvp.client.proxy.PlaceRequest;
+import com.smartgwt.client.widgets.grid.ListGridRecord;
 
 public class RelatedResourceListItem extends RelatedResourceBaseListItem<RelatedResourceDto> {
 
@@ -18,5 +21,17 @@ public class RelatedResourceListItem extends RelatedResourceBaseListItem<Related
     @Override
     protected List<PlaceRequest> buildLocation(RelatedResourceDto relatedResourceDto) {
         return PlaceRequestUtils.buildAbsoluteResourcePlaceRequest(relatedResourceDto);
+    }
+
+    public List<String> getSelectedRelatedResourceUrns() {
+        List<String> urns = new ArrayList<String>();
+        ListGridRecord[] records = listGrid.getRecords();
+        if (records != null) {
+            for (ListGridRecord record : records) {
+                RelatedResourceBaseRecord relatedResourceRecord = (RelatedResourceBaseRecord) record;
+                urns.add(relatedResourceRecord.getUrn());
+            }
+        }
+        return urns;
     }
 }
