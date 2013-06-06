d6dcaab6835951113db5d3bf511ed83c08122fd6
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-06 09:17:28 +0000

    METAMAC-1705 ERROR: no permitir modificar metadatos SDMX de recursos importados
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@46534 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/category/widgets/CategorySchemeMainFormLayout.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/category/widgets/CategorySchemeMainFormLayout.java
@@ -76,7 +76,7 @@ public class CategorySchemeMainFormLayout extends LifeCycleMainFormLayout {
     }
     @Override
     protected void showCancelValidityButton() {
-        if (CategoriesClientSecurityUtils.canCancelCategorySchemeValidity(maintainer, versionLogic)) {
+        if (CategoriesClientSecurityUtils.canCancelCategorySchemeValidity(urn, maintainer, versionLogic)) {
             cancelValidity.show();
         }
     }
@@ -111,6 +111,6 @@ public class CategorySchemeMainFormLayout extends LifeCycleMainFormLayout {
 
     private boolean canCategorySchemeBeVersion() {
         // Resources from other maintainers can not be version
-        return org.siemac.metamac.srm.web.client.utils.CommonUtils.isDefaultOrRootMaintainer(maintainer) && CategoriesClientSecurityUtils.canVersioningCategoryScheme();
+        return org.siemac.metamac.srm.web.client.utils.CommonUtils.isDefaultMaintainer(maintainer) && CategoriesClientSecurityUtils.canVersioningCategoryScheme();
     }
 }
