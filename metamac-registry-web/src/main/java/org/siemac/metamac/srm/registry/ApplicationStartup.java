d95be78c8e9a28ecea2d6e00d5a30f3440ba0075
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-07 12:30:41 +0000

    IN PROGRESS - issue LLCC-75: API: SDMX externa http://jira.arte-consultores.com/browse/LLCC-75
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@46602 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-registry-web/src/main/java/org/siemac/metamac/srm/registry/ApplicationStartup.java
@@ -0,0 +1,55 @@
+package org.siemac.metamac.srm.registry;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.siemac.metamac.core.common.conf.ConfigurationService;
+import org.siemac.metamac.core.common.util.ApplicationContextProvider;
+
+import com.arte.statistic.sdmx.srm.rest.external.conf.SrmConfigurationConstants;
+
+public class ApplicationStartup implements ServletContextListener {
+
+    private static final Log     LOG = LogFactory.getLog(ApplicationStartup.class);
+
+    private ConfigurationService configurationService;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            configurationService = ApplicationContextProvider.getApplicationContext().getBean(ConfigurationService.class);
+            checkConfiguration();
+        } catch (Exception e) {
+            // Abort startup application
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void checkConfiguration() {
+        LOG.info("**********************************************************");
+        LOG.info("Checking application configuration");
+        LOG.info("**********************************************************");
+
+        // Datasource
+        configurationService.checkRequiredProperty(SrmConfigurationConstants.DB_DRIVER_NAME);
+        configurationService.checkRequiredProperty(SrmConfigurationConstants.DB_URL);
+        configurationService.checkRequiredProperty(SrmConfigurationConstants.DB_USERNAME);
+        configurationService.checkRequiredProperty(SrmConfigurationConstants.DB_PASSWORD);
+        configurationService.checkRequiredProperty(SrmConfigurationConstants.DB_DIALECT);
+
+        // Api
+        configurationService.checkRequiredProperty(SrmConfigurationConstants.METAMAC_ORGANISATION_URN);
+        configurationService.checkRequiredProperty(SrmConfigurationConstants.METAMAC_ORGANISATION);
+
+        LOG.info("**********************************************************");
+        LOG.info("Application configuration checked");
+        LOG.info("**********************************************************");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+}
