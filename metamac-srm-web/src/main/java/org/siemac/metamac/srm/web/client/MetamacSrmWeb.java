e61710864dd222222648b95253822c180b1e962b
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-05 07:47:25 +0000

    Refactor
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@47284 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/MetamacSrmWeb.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/MetamacSrmWeb.java
@@ -9,30 +9,11 @@ import org.siemac.metamac.srm.web.client.gin.MetamacSrmWebGinjector;
 import org.siemac.metamac.srm.web.shared.organisation.GetDefaultMaintainerAction;
 import org.siemac.metamac.srm.web.shared.organisation.GetDefaultMaintainerResult;
 import org.siemac.metamac.sso.client.MetamacPrincipal;
-import org.siemac.metamac.web.common.client.MetamacEntryPoint;
 import org.siemac.metamac.web.common.client.MetamacSecurityEntryPoint;
-import org.siemac.metamac.web.common.client.events.LoginAuthenticatedEvent;
 import org.siemac.metamac.web.common.client.gin.MetamacWebGinjector;
-import org.siemac.metamac.web.common.client.utils.ApplicationEditionLanguages;
-import org.siemac.metamac.web.common.client.utils.ApplicationOrganisation;
-import org.siemac.metamac.web.common.client.widgets.MetamacNavBar;
 import org.siemac.metamac.web.common.client.widgets.WaitingAsyncCallback;
-import org.siemac.metamac.web.common.shared.GetLoginPageUrlAction;
-import org.siemac.metamac.web.common.shared.GetLoginPageUrlResult;
-import org.siemac.metamac.web.common.shared.GetNavigationBarUrlAction;
-import org.siemac.metamac.web.common.shared.GetNavigationBarUrlResult;
-import org.siemac.metamac.web.common.shared.LoadConfigurationPropertiesAction;
-import org.siemac.metamac.web.common.shared.LoadConfigurationPropertiesResult;
-import org.siemac.metamac.web.common.shared.MockCASUserAction;
-import org.siemac.metamac.web.common.shared.MockCASUserResult;
-import org.siemac.metamac.web.common.shared.ValidateTicketAction;
-import org.siemac.metamac.web.common.shared.ValidateTicketResult;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Document;
-import com.google.gwt.http.client.UrlBuilder;
-import com.google.gwt.user.client.Window;
-import com.gwtplatform.mvp.client.DelayedBindRegistry;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -55,11 +36,11 @@ public class MetamacSrmWeb extends MetamacSecurityEntryPoint {
     @Override
     public void onModuleLoad() {
         setUncaughtExceptionHandler();
-        
+
         prepareApplication(SECURITY_ENABLED);
-        
+
     }
-    
+
     @Override
     protected void onBeforeLoadApplication() {
         ginjector.getDispatcher().execute(new GetDefaultMaintainerAction(), new WaitingAsyncCallback<GetDefaultMaintainerResult>() {
@@ -78,7 +59,7 @@ public class MetamacSrmWeb extends MetamacSecurityEntryPoint {
             }
         });
     }
-    
+
     public static OrganisationMetamacDto getDefaultMaintainer() {
         return defaultMaintainer;
     }
@@ -111,31 +92,29 @@ public class MetamacSrmWeb extends MetamacSecurityEntryPoint {
     public static void showErrorPage() {
         ginjector.getPlaceManager().revealErrorPlace(null);
     }
-    
-    //Security Entry point 
-    
+
+    // Security Entry point
+
     @Override
     protected String getApplicationTitle() {
         return getConstants().appTitle();
     }
-    
+
     @Override
     protected MetamacPrincipal getPrincipal() {
         return principal;
     }
-    
+
     @Override
     protected void setPrincipal(MetamacPrincipal principal) {
-        this.principal = principal;
+        MetamacSrmWeb.principal = principal;
     }
-    
+
     @Override
     protected String getSecurityApplicationId() {
         return SrmConstants.SECURITY_APPLICATION_ID;
     };
-    
- 
-    
+
     @Override
     protected MetamacWebGinjector getWebGinjector() {
         return ginjector;
