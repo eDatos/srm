0a49dcbbe4aed6e58de7fe2943a1696bcf9f47d0
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-11 11:20:04 +0000

    METAMAC-1788 METAMAC-1746 Mostrar el codeFull de las categorías
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@47480 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/GetRelatedResourcesActionHandler.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/GetRelatedResourcesActionHandler.java
@@ -30,7 +30,6 @@ import org.siemac.metamac.web.common.shared.exception.MetamacWebException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.arte.statistic.sdmx.v2_1.domain.dto.category.CategoryRelatedResourceDto;
 import com.arte.statistic.sdmx.v2_1.domain.dto.common.RelatedResourceDto;
 import com.gwtplatform.dispatch.shared.ActionException;
 
@@ -278,16 +277,16 @@ public class GetRelatedResourcesActionHandler extends SecurityActionHandler<GetR
                 case CATEGORIES_FOR_CATEGORISATIONS: {
                     CategoryWebCriteria categoryWebCriteria = (CategoryWebCriteria) action.getCriteria();
                     criteria.setRestriction(MetamacWebCriteriaUtils.getCategoryCriteriaRestriction(categoryWebCriteria));
-                    MetamacCriteriaResult<CategoryRelatedResourceDto> categoriesResult = srmCoreServiceFacade.findCategoriesCanBeCategorisationCategoryByCondition(
+                    MetamacCriteriaResult<RelatedResourceDto> categoriesResult = srmCoreServiceFacade.findCategoriesCanBeCategorisationCategoryByCondition(
                             ServiceContextHolder.getCurrentServiceContext(), criteria);
 
                     // Transform the categories result into a related resource result
 
-                    List<CategoryRelatedResourceDto> categories = categoriesResult.getResults();
+                    List<RelatedResourceDto> categories = categoriesResult.getResults();
                     List<RelatedResourceDto> relatedResourceDtos = new ArrayList<RelatedResourceDto>();
-                    for (CategoryRelatedResourceDto categoryRelatedResourceDto : categories) {
+                    for (RelatedResourceDto categoryRelatedResourceDto : categories) {
                         RelatedResourceDto relatedResourceDto = categoryRelatedResourceDto;
-                        relatedResourceDto.setCode(categoryRelatedResourceDto.getCodeFull()); // Set the codeFull into the code field
+                        relatedResourceDto.setCode(categoryRelatedResourceDto.getCodeNested()); // Set the codeFull into the code field
                         relatedResourceDtos.add(relatedResourceDto);
                     }
 
