a52e310d2e2bb5dde004c7042d57d343b894446f
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-18 11:38:08 +0000

    METAMAC-1257 Establecer el lenguaje de la aplicación mediante la detección del mismo en el navegador
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@47687 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/rest/StatisticalOperationsRestInternalFacadeImpl.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/rest/StatisticalOperationsRestInternalFacadeImpl.java
@@ -1,5 +1,6 @@
 package org.siemac.metamac.srm.web.server.rest;
 
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -109,8 +110,8 @@ public class StatisticalOperationsRestInternalFacadeImpl implements StatisticalO
     }
 
     private void throwMetamacWebException(ServiceContext serviceContext, String exceptionCode) throws MetamacWebException {
-        String locale = (String) serviceContext.getProperty(LocaleConstants.locale);
-        String exceptionnMessage = webTranslateExceptions.getTranslatedMessage(exceptionCode, locale);
+        Locale locale = (Locale) serviceContext.getProperty(LocaleConstants.locale);
+        String exceptionnMessage = webTranslateExceptions.getTranslatedMessage(exceptionCode, locale.getLanguage());
 
         throw new MetamacWebException(exceptionCode, exceptionnMessage);
     }
