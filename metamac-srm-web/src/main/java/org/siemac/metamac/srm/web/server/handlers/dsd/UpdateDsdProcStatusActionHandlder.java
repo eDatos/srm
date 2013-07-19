c950b115665c10774e88bf05a0f6cd44cd8dc30e
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-19 15:07:32 +0000

    Corregido imports
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@47917 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/dsd/UpdateDsdProcStatusActionHandlder.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/handlers/dsd/UpdateDsdProcStatusActionHandlder.java
@@ -1,5 +1,7 @@
 package org.siemac.metamac.srm.web.server.handlers.dsd;
 
+import java.util.Locale;
+
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.core.common.lang.shared.LocaleConstants;
@@ -83,7 +85,7 @@ public class UpdateDsdProcStatusActionHandlder extends SecurityActionHandler<Upd
     }
 
     private void throwMetamacWebException(ServiceContext serviceContext, String exceptionCode) throws MetamacWebException {
-        String locale = (String) serviceContext.getProperty(LocaleConstants.locale);
+        Locale locale = (Locale) serviceContext.getProperty(LocaleConstants.locale);
         String exceptionnMessage = webTranslateExceptions.getTranslatedMessage(exceptionCode, locale);
 
         throw new MetamacWebException(exceptionCode, exceptionnMessage);
