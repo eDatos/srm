4712bfa95aa3fb59da594efbea8eebd4ac7d6cbe
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-26 12:33:15 +0000

    Corregido error al hacer split en la URN de las categorías temporales
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@48129 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/utils/PlaceRequestUtils.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/client/utils/PlaceRequestUtils.java
@@ -291,14 +291,6 @@ public class PlaceRequestUtils {
 
     // Categories
 
-    /**
-     * Splits a category identifier (not code!) by its version.
-     * For example: the identifier SDMX_AGENCY:category_scheme_0001(1.0).category1.category2 is split in two values:
-     * · SDMX_AGENCY:category_scheme_0001
-     * · category1.category2
-     */
-    public static final String CATEGORY_IDENTIFIER_SPLITTER_BY_VERSION_REG_EXP = "\\(\\d+\\.\\d+\\)\\.";
-
     public static String getCategoryParamFromUrl(PlaceManager placeManager) {
         return getParamFromUrl(placeManager, NameTokens.categoryPage, PlaceRequestParams.categoryParamId);
     }
@@ -311,8 +303,7 @@ public class PlaceRequestUtils {
     }
 
     public static PlaceRequest buildRelativeCategoryPlaceRequest(String categoryUrn) {
-        String identifier = UrnUtils.removePrefix(categoryUrn);
-        String[] splitIdentifier = identifier.split(CATEGORY_IDENTIFIER_SPLITTER_BY_VERSION_REG_EXP);
+        String[] splitIdentifier = UrnUtils.splitUrnItem(categoryUrn);
         PlaceRequest placeRequest = new PlaceRequest(NameTokens.categoryPage).with(PlaceRequestParams.categoryParamId, splitIdentifier[splitIdentifier.length - 1]);
         return placeRequest;
     }
