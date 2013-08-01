5f2e56d2477712e0b303634f29980c6c6780fa8f
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-01 13:10:12 +0000

    Mover utilidades para la subida de ficheros a metamac-web-common
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@48307 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/dsd/listener/UploadListener.java
+++
@@ -1,7 +0,0 @@
-package org.siemac.metamac.srm.web.dsd.listener;
-
-public interface UploadListener {
-
-    public void uploadComplete(String fileName);
-    public void uploadFailed(String errorMessage);
-}
