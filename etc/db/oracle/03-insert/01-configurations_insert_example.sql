b276954ec33d85d1b08ea56eb7fd96f5a3a5db4e
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-11-13 13:29:28 +0000

    METAMAC-1969: Configuraciones de aplicaciones en Base de datos en vez de en archivo de propiedades http://jira.arte-consultores.com/browse/METAMAC-1969
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@49974 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ etc/db/oracle/03-insert/01-configurations_insert_example.sql
@@ -0,0 +1,20 @@
+-- ###########################################
+-- # insert
+-- ###########################################
+
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.variables.variable_world.urn','urn:siemac:org.siemac.metamac.infomodel.structuralresources.Variable=TERRITORIO_MUNDO');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.variables.variable_element_world.urn','urn:siemac:org.siemac.metamac.infomodel.structuralresources.VariableElement=TERRITORIO_MUNDO.MUNDO');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.jobs.delete_deprecated_entities.cron_expression','0 30 2 ? * *');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.dsd.time_dimension_or_attribute.default_concept.urn',null);
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.dsd.primary_measure.default_concept.urn',null);
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.dsd.measure_dimension_or_attribute.default_concept.urn',null);
+
+-- DATASOURCE: ORACLE
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.db.url','jdbc:oracle:thin:@FILL_ME_WITH_HOST:FILL_ME_WITH_PORT:XE');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.db.username','FILL_ME_WITH_USERNAME');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.db.password','FILL_ME_WITH_PASSWORD');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.db.driver_name','oracle.jdbc.OracleDriver');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.db.dialect','org.siemac.metamac.hibernate.dialect.Oracle10gDialectMetamac');
+
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.srm.user_guide.file_name','Gestor_accesos-Manual_usuario.pdf');
+
