4aed1865f0b469cd7cc615338e3f7f1b140d5498
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-08-18 08:37:15 +0000

    METAMAC-2266 Centralizar la gestión de errores producidos por invocaciones a las APIS
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-srm/trunk/metamac-srm@52975 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/rest/StatisticalOperationsRestInternalFacadeImpl.java
+++ metamac-srm-web/src/main/java/org/siemac/metamac/srm/web/server/rest/StatisticalOperationsRestInternalFacadeImpl.java
@@ -1,24 +1,16 @@
 package org.siemac.metamac.srm.web.server.rest;
 
-import java.util.Locale;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.cxf.jaxrs.client.ServerWebApplicationException;
-import org.apache.cxf.jaxrs.client.WebClient;
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
-import org.siemac.metamac.core.common.lang.shared.LocaleConstants;
 import org.siemac.metamac.rest.common.v1_0.domain.ComparisonOperator;
 import org.siemac.metamac.rest.common.v1_0.domain.LogicalOperator;
 import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.Operation;
 import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.OperationCriteriaPropertyRestriction;
 import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.Operations;
-import org.siemac.metamac.srm.web.shared.WebMessageExceptionsConstants;
-import org.siemac.metamac.web.common.server.utils.WebExceptionUtils;
+import org.siemac.metamac.srm.core.common.error.ServiceExceptionParameters;
+import org.siemac.metamac.web.common.server.rest.utils.RestExceptionUtils;
 import org.siemac.metamac.web.common.server.utils.WebTranslateExceptions;
-import org.siemac.metamac.web.common.shared.constants.CommonSharedConstants;
 import org.siemac.metamac.web.common.shared.exception.MetamacWebException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -26,10 +18,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class StatisticalOperationsRestInternalFacadeImpl implements StatisticalOperationsRestInternalFacade {
 
-    private static Logger          logger = Logger.getLogger(StatisticalOperationsRestInternalFacadeImpl.class.getName());
-
     @Autowired
     private RestApiLocator         restApiLocator;
+
+    @Autowired
+    private RestExceptionUtils     restExceptionUtils;
 
     @Autowired
     private WebTranslateExceptions webTranslateExceptions;
@@ -38,12 +31,8 @@ public class StatisticalOperationsRestInternalFacadeImpl implements StatisticalO
     public Operation retrieveOperation(ServiceContext serviceContext, String operationCode) throws MetamacWebException {
         try {
             return restApiLocator.getStatisticalOperationsRestFacadeV10().retrieveOperationById(operationCode); // OPERATION ID in the rest API is what we call CODE
-        } catch (ServerWebApplicationException e) {
-            throwMetamacWebExceptionFromServerWebApplicationException(serviceContext, e);
-            return null;
         } catch (Exception e) {
-            logger.log(Level.SEVERE, e.getMessage());
-            throw new MetamacWebException(CommonSharedConstants.EXCEPTION_UNKNOWN, e.getMessage());
+            throw manageRestException(serviceContext, e);
         }
     }
 
@@ -56,12 +45,8 @@ public class StatisticalOperationsRestInternalFacadeImpl implements StatisticalO
 
             Operations findOperationsResult = restApiLocator.getStatisticalOperationsRestFacadeV10().findOperations(query, null, limit, offset);
             return findOperationsResult;
-        } catch (ServerWebApplicationException e) {
-            throwMetamacWebExceptionFromServerWebApplicationException(serviceContext, e);
-            return null;
         } catch (Exception e) {
-            logger.log(Level.SEVERE, e.getMessage());
-            throw new MetamacWebException(CommonSharedConstants.EXCEPTION_UNKNOWN, e.getMessage());
+            throw manageRestException(serviceContext, e);
         }
     }
 
@@ -92,27 +77,7 @@ public class StatisticalOperationsRestInternalFacadeImpl implements StatisticalO
     // EXCEPTION HANDLERS
     //
 
-    private void throwMetamacWebExceptionFromServerWebApplicationException(ServiceContext serviceContext, ServerWebApplicationException e) throws MetamacWebException {
-        logger.log(Level.SEVERE, e.getMessage());
-
-        org.siemac.metamac.rest.common.v1_0.domain.Exception exception = e.toErrorObject(WebClient.client(restApiLocator.getStatisticalOperationsRestFacadeV10()),
-                org.siemac.metamac.rest.common.v1_0.domain.Exception.class);
-
-        if (exception == null) {
-            if (e.getResponse().getStatus() == 404) {
-                throwMetamacWebException(serviceContext, WebMessageExceptionsConstants.REST_API_STATISTICAL_OPERATIONS_INVOCATION_ERROR_404);
-            } else {
-                throwMetamacWebException(serviceContext, WebMessageExceptionsConstants.REST_API_STATISTICAL_OPERATIONS_INVOCATION_ERROR_UNKNOWN);
-            }
-        }
-
-        throw WebExceptionUtils.createMetamacWebException(exception);
-    }
-
-    private void throwMetamacWebException(ServiceContext serviceContext, String exceptionCode) throws MetamacWebException {
-        Locale locale = (Locale) serviceContext.getProperty(LocaleConstants.locale);
-        String exceptionnMessage = webTranslateExceptions.getTranslatedMessage(exceptionCode, locale);
-
-        throw new MetamacWebException(exceptionCode, exceptionnMessage);
+    private MetamacWebException manageRestException(ServiceContext ctx, Exception e) throws MetamacWebException {
+        return restExceptionUtils.manageMetamacRestException(ctx, e, ServiceExceptionParameters.API_SRM_INTERNAL, restApiLocator.getStatisticalOperationsRestFacadeV10());
     }
 }
