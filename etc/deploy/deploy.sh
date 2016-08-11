560cc51da172a1bd63f013aed9926f864552dcb6
Zebensui Méndez Corujo <zmencor@arte-consultores.com>
2016-08-11 14:44:21 +0100

    METAMAC-2475 - Migración de las aplicaciones de Tomcat6 a Tomcat8 y JDK8
    - Deploy

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-TMP_PATH=/servers/metamac/tmp
+HOME_PATH=metamac-structural-resources
+TRANSFER_PATH=$HOME_PATH/tmp
 DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
 ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/metamac/environment.xml
 LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
 
-scp -r etc/deploy deploy@estadisticas.arte-consultores.com:
-scp metamac-srm-web/target/structural-resources-internal-*.war deploy@estadisticas.arte-consultores.com:structural-resources-internal.war
-scp metamac-srm-external-web/target/structural-resources-*.war deploy@estadisticas.arte-consultores.com:structural-resources.war
+scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
+scp metamac-srm-web/target/structural-resources-internal-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/structural-resources-internal.war
+scp metamac-srm-external-web/target/structural-resources-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/structural-resources.war
 ssh deploy@estadisticas.arte-consultores.com <<EOF
 
-    chmod a+x deploy/*.sh;
-    . deploy/utilities.sh
+    chmod a+x $TRANSFER_PATH/deploy/*.sh;
+    . $TRANSFER_PATH/deploy/utilities.sh
     
     sudo service metamac01 stop
     checkPROC "metamac"
@@ -19,35 +20,31 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
     ###
     # STRUCTURAL-RESOURCES-INTERNAL
     ###
-    # Backup Configuration
-    sudo mv $DEPLOY_TARGET_PATH/structural-resources-internal/$ENVIRONMENT_RELATIVE_PATH_FILE $TMP_PATH/environment.xml_structural-resources-internal_tmp
-    sudo mv $DEPLOY_TARGET_PATH/structural-resources-internal/$LOGBACK_RELATIVE_PATH_FILE $TMP_PATH/logback.xml_structural-resources-internal_tmp
     
     # Update Process
-    sudo mv structural-resources-internal.war $DEPLOY_TARGET_PATH/structural-resources-internal.war
-    sudo unzip $DEPLOY_TARGET_PATH/structural-resources-internal.war -d $DEPLOY_TARGET_PATH
+    sudo rm -rf $DEPLOY_TARGET_PATH/structural-resources-internal
+    sudo mv $TRANSFER_PATH/structural-resources-internal.war $DEPLOY_TARGET_PATH/structural-resources-internal.war
+    sudo unzip $DEPLOY_TARGET_PATH/structural-resources-internal.war -d $DEPLOY_TARGET_PATH/structural-resources-internal
     sudo rm -rf $DEPLOY_TARGET_PATH/structural-resources-internal.war
     
     # Restore Configuration
-    sudo mv $TMP_PATH/environment.xml_structural-resources-internal_tmp $DEPLOY_TARGET_PATH/structural-resources-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
-    sudo mv $TMP_PATH/logback.xml_structural-resources-internal_tmp $DEPLOY_TARGET_PATH/structural-resources-internal/$LOGBACK_RELATIVE_PATH_FILE
+    sudo cp $HOME_PATH/environment_internal.xml $DEPLOY_TARGET_PATH/structural-resources-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo cp $HOME_PATH/logback_internal.xml $DEPLOY_TARGET_PATH/structural-resources-internal/$LOGBACK_RELATIVE_PATH_FILE
     
     
     ###
-    # STRUCTURAL-RESOURCES
+    # STRUCTURAL-RESOURCES-EXTERNA
     ###
-    # Backup Configuration
-    sudo mv $DEPLOY_TARGET_PATH/structural-resources/$ENVIRONMENT_RELATIVE_PATH_FILE $TMP_PATH/environment.xml_structural-resources_tmp
-    sudo mv $DEPLOY_TARGET_PATH/structural-resources/$LOGBACK_RELATIVE_PATH_FILE $TMP_PATH/logback.xml_structural-resources_tmp
     
     # Update Process
-    sudo mv structural-resources.war $DEPLOY_TARGET_PATH/structural-resources.war
-    sudo unzip $DEPLOY_TARGET_PATH/structural-resources.war -d $DEPLOY_TARGET_PATH
+    sudo rm -rf $DEPLOY_TARGET_PATH/structural-resources
+    sudo mv $TRANSFER_PATH/structural-resources.war $DEPLOY_TARGET_PATH/structural-resources.war
+    sudo unzip $DEPLOY_TARGET_PATH/structural-resources.war -d $DEPLOY_TARGET_PATH/structural-resources
     sudo rm -rf $DEPLOY_TARGET_PATH/structural-resources.war
     
     # Restore Configuration
-    sudo mv $TMP_PATH/environment.xml_structural-resources_tmp $DEPLOY_TARGET_PATH/structural-resources/$ENVIRONMENT_RELATIVE_PATH_FILE
-    sudo mv $TMP_PATH/logback.xml_structural-resources_tmp $DEPLOY_TARGET_PATH/structural-resources/$LOGBACK_RELATIVE_PATH_FILE
+    sudo cp $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH/structural-resources/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo cp $HOME_PATH/logback.xml $DEPLOY_TARGET_PATH/structural-resources/$LOGBACK_RELATIVE_PATH_FILE
     
     sudo chown -R metamac.metamac /servers/metamac
     sudo service metamac01 start
